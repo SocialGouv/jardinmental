d63359e52d48a22fc9e76c801279c9a2a9222977
Alezco <benjaminmorali4@hotmail.fr>
2020-10-26 17:50:07 +0100

    feat(navigation): add react-navigation

---
+++ src/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+  BLUE: '#26387C',
+  LIGHT_BLUE: '#1FC6D5',
+  DARK_BLUE: '#0A215C',
+};
--- src/diary/patient-state-item.js
+++ src/diary/patient-state-item.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
+const PatientStateItem = ({patientStateItem, category}) => {
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          ...styles.iconContainer,
+          backgroundColor: patientStateItem.color,
+        }}>
+        <patientStateItem.icon width={20} height={20} />
+      </View>
+      <Text style={styles.categoryText}>{category}</Text>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
@@ -23,20 +38,5 @@ const styles = StyleSheet.create({
     fontSize: 15,
   },
 });
-
-const PatientStateItem = ({patientStateItem, category}) => {
-  return (
-    <View style={styles.container}>
-      <View
-        style={{
-          ...styles.iconContainer,
-          backgroundColor: patientStateItem.color,
-        }}>
-        <patientStateItem.icon width={20} height={20} />
-      </View>
-      <Text style={styles.categoryText}>{category}</Text>
-    </View>
-  );
-};
 
 export default PatientStateItem;
