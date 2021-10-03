df6b6dcc6b6eda6af3d17af7b8cff59bc37db4bf
tangimds <tangimds@gmail.com>
2021-10-03 20:02:40 +0200

    utils

--- src/scenes/diary/fake-diary-data.js
+++ src/scenes/diary/fake-diary-data.js
@@ -2,7 +2,7 @@ import {categoryStates, frequence} from '../../utils/constants';
 import {addDays} from 'date-fns';
 import {formatDay, oneDay} from '../../utils/date/helpers';
 
-const MAX_DAY = 12;
+const MAX_DAY = 10;
 export const startDate = new Date(Date.now() - MAX_DAY * oneDay);
 
 export const fakeDiaryData = {
@@ -82,7 +82,17 @@ export const fakeDiaryData = {
     SLEEP: categoryStates.VERY_BAD,
     NOTES: null,
   },
-  [formatDay(addDays(startDate, 5))]: null,
+  [formatDay(addDays(startDate, 5))]: {
+    MOOD: categoryStates.MIDDLE,
+    ANXIETY_FREQUENCE: frequence.MANY_TIMES,
+    ANXIETY_INTENSITY: categoryStates.VERY_BAD,
+    BADTHOUGHTS_FREQUENCE: frequence.MANY_TIMES,
+    BADTHOUGHTS_INTENSITY: categoryStates.VERY_GOOD,
+    SENSATIONS_FREQUENCE: frequence.MANY_TIMES,
+    SENSATIONS_INTENSITY: categoryStates.GOOD,
+    SLEEP: categoryStates.MIDDLE,
+    NOTES: '',
+  },
   [formatDay(addDays(startDate, 6))]: {
     MOOD: categoryStates.VERY_GOOD,
     ANXIETY_FREQUENCE: frequence.MANY_TIMES,
@@ -147,7 +157,7 @@ export const fakeDiaryData = {
       'This is a very long note. This is a very long note. This is a very long note. This is a very long note. This is a very long note. This is a very long note. This is a very long note. ',
   },
   [formatDay(addDays(startDate, 7))]: {
-    MOOD: categoryStates.VERY_GOOD,
+    MOOD: categoryStates.BAD,
     ANXIETY_FREQUENCE: frequence.MANY_TIMES,
     ANXIETY_INTENSITY: categoryStates.VERY_BAD,
     BADTHOUGHTS_FREQUENCE: frequence.MANY_TIMES,
@@ -158,7 +168,7 @@ export const fakeDiaryData = {
     NOTES: '',
   },
   [formatDay(addDays(startDate, 8))]: {
-    MOOD: categoryStates.VERY_GOOD,
+    MOOD: categoryStates.GOOD,
     ANXIETY_FREQUENCE: frequence.MANY_TIMES,
     ANXIETY_INTENSITY: categoryStates.VERY_BAD,
     BADTHOUGHTS_FREQUENCE: frequence.MANY_TIMES,
@@ -169,7 +179,7 @@ export const fakeDiaryData = {
     NOTES: {},
   },
   [formatDay(addDays(startDate, 9))]: {
-    MOOD: categoryStates.VERY_GOOD,
+    MOOD: categoryStates.GOOD,
     ANXIETY_FREQUENCE: frequence.MANY_TIMES,
     ANXIETY_INTENSITY: categoryStates.VERY_BAD,
     BADTHOUGHTS_FREQUENCE: frequence.MANY_TIMES,
@@ -218,8 +228,8 @@ export const fakeDiaryData = {
   },
   [formatDay(addDays(startDate, 10))]: {
     MOOD: categoryStates.VERY_GOOD,
-    ANXIETY_FREQUENCE: frequence.MANY_TIMES,
-    ANXIETY_INTENSITY: categoryStates.VERY_BAD,
+    ANXIETY_FREQUENCE: frequence.SEVERAL_TIMES,
+    ANXIETY_INTENSITY: categoryStates.GOOD,
     BADTHOUGHTS_FREQUENCE: frequence.MANY_TIMES,
     BADTHOUGHTS_INTENSITY: categoryStates.VERY_GOOD,
     SENSATIONS_FREQUENCE: frequence.MANY_TIMES,
--- src/utils/date/helpers.js
+++ src/utils/date/helpers.js
@@ -96,3 +96,20 @@ export const displayOnlyHourAndMinute = (timeString) => {
   const [hours, minutes] = timeString.split(':');
   return `${hours}:${minutes}`;
 };
+
+export const changeTimezone = (date, ianatz) => {
+  // suppose the date is 12:00 UTC
+  var invdate = new Date(
+    date.toLocaleString('fr-FR', {
+      timeZone: ianatz,
+    }),
+  );
+  console.log(invdate);
+
+  // then invdate will be 07:00 in Toronto
+  // and the diff is 5 hours
+  var diff = date.getTime() - invdate.getTime();
+
+  // so 12:00 in Toronto is 17:00 UTC
+  return new Date(date.getTime() - diff); // needs to substract
+};
