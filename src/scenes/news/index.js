5c64a848639ddb5f897ba0b53aaea4931165487d
tangimds <tangimds@gmail.com>
2021-11-12 11:22:53 +0100

    Update index.js

--- src/scenes/news/index.js
+++ src/scenes/news/index.js
@@ -13,7 +13,7 @@ export const getBadgeNotesVersion = async () => {
   return lastNotesVersion !== LAST_NOTES_VERSION;
 };
 
-export const LAST_NOTES_VERSION = '1.14';
+export const LAST_NOTES_VERSION = '1.15.6';
 
 export default ({navigation}) => {
   useEffect(() => {
