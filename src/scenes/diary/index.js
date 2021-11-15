0fbcde2eaef24abbc30cf6792f8d168c242a4553
tangimds <tangimds@gmail.com>
2021-11-15 17:14:25 +0100

    up

--- src/scenes/diary/index.js
+++ src/scenes/diary/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
 
 import Text from '../../components/MyText';
 
 export default () => {
-  return <View></View>;
+  return (
+    <SafeAreaView style={styles.safe}>
+      <Text>Bientôt disponible !</Text>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  safe: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+});
