2ae2777a2f53f818c95233770bba1853c0a36c59
tangimds <tangimds@gmail.com>
2021-12-16 17:59:45 +0100

    fix(contact): regrouper les informations et les contacts

--- src/scenes/contact/index.js
+++ src/scenes/contact/index.js
@@ -1,11 +1,15 @@
-import React, {useEffect} from 'react';
-import {StyleSheet, SafeAreaView, ScrollView, Linking} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, SafeAreaView, ScrollView, Linking, View } from 'react-native';
 import Text from '../../components/MyText';
-import {colors} from '../../utils/colors';
+import { colors } from '../../utils/colors';
 import logEvents from '../../services/logEvents';
 import BackButton from '../../components/BackButton';
+import InfoSvg from '../../../assets/svg/info.svg';
 
-const Contact = ({navigation}) => {
+const Contact = ({ navigation }) => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+
   useEffect(() => {
     const unsubscribe = navigation.addListener('tabPress', (e) => {
       logEvents.logContactOpen();
@@ -64,12 +68,157 @@ const Contact = ({navigation}) => {
             0 800 235 236
           </Text>
         </Text>
+        <View style={styles.containerInfos}>
+          <InfoSvg />
+          <View paddingLeft={10} style={styles.textContainer}>
+            <Text style={styles.titleInfos}>
+              Les troubles psychiatriques se soignent
+            </Text>
+            {isFullScreen ? (
+              <>
+                <Text style={styles.explanation}>
+                  Quand on a un doute, quand on se pose la question, c’est
+                  toujours une bonne idée de consulter un professionnel de
+                  santé, qui saura nous informer et nous orienter.{'\n\n'}
+                  La santé mentale est une composante de la santé. Prendre soin
+                  de soi passe aussi par le fait de prendre soin de sa santé
+                  mentale.
+                  {'\n'}
+                  Il arrive qu’à certains moments de la vie, on se sente moins
+                  bien psychiquement. Notre santé mentale est alors moins bonne
+                  et l’on peut présenter un trouble psychiatrique. Les troubles
+                  psychiatriques sont très fréquents, ils peuvent toucher
+                  n’importe qui à n’importe quel moment de la vie, surtout quand
+                  on se retrouve dans des situations difficiles.{'\n\n'} Le fait
+                  d’avoir un trouble psychiatrique n’a aucun rapport avec une
+                  quelconque faiblesse ou manque de volonté. C’est juste que
+                  parfois, pour pleins de raisons différentes, notre santé
+                  mentale est moins bonne, comme n’importe quelle autre
+                  composante de notre santé.
+                  {'\n'} Les troubles psychiatriques se soignent, encore plus
+                  s’ils sont pris en charge rapidement. Quand on a un doute,
+                  quand on se pose la question, c’est toujours une bonne idée de
+                  consulter un professionnel de santé, qui saura nous informer
+                  et nous orienter.
+                </Text>
+                <Text style={styles.lessButton} onPress={() => setIsFullScreen(e => !e)}>
+                  Reduire
+                </Text>
+              </>
+            ) : (
+              <>
+                <Text numberOfLines={5} style={styles.explanation}>
+                  Quand on a un doute, quand on se pose la question, c’est
+                  toujours une bonne idée de consulter un professionnel de
+                  santé, qui saura nous informer et nous orienter.
+                </Text>
+                <Text style={styles.moreButton} onPress={() => setIsFullScreen(e => !e)}>
+                  Afficher la suite
+                </Text>
+              </>
+            )}
+          </View>
+        </View>
+        <Text style={styles.content}>
+          La santé mentale, qu’est-ce que c’est ?{'\n'}
+          <Text
+            style={styles.link}
+            onPress={() => {
+              logEvents.logInfoClick('en_savoir_plus_mentale');
+              Linking.openURL(
+                'https://www.psycom.org/comprendre/la-sante-mentale/on-a-toutes-et-tous-une-sante-mentale/',
+              );
+            }}>
+            en savoir plus
+          </Text>
+          {'\n\n'}
+          Avoir un trouble psychiatrique, c’est fréquent ? 1 personne sur 4
+          souffre de troubles mentaux{'\n'}
+          <Text
+            style={styles.link}
+            onPress={() => {
+              logEvents.logInfoClick('article_OMS');
+              Linking.openURL(
+                'https://www.who.int/whr/2001/media_centre/press_release/fr/',
+              );
+            }}>
+            Article de l'OMS
+          </Text>
+          {'\n\n'}
+          On a tous besoin d'aide à un moment ou un autre de notre vie.{'\n'}
+          <Text
+            style={styles.link}
+            onPress={() => {
+              logEvents.logInfoClick('voir_un_psy');
+              Linking.openURL('https://pasapas-jeunes.com/j-ai-besoin-d-aide');
+            }}>
+            C’est grave d’aller voir un psy ?{' '}
+          </Text>
+          {'\n\n'}
+          Quels sont les différents troubles psy ?{'\n'}
+          <Text
+            style={styles.link}
+            onPress={() => {
+              logEvents.logInfoClick('en_savoir_plus_troubles');
+              Linking.openURL(
+                'https://www.psycom.org/comprendre/la-sante-mentale/les-troubles-psy/',
+              );
+            }}>
+            en savoir plus
+          </Text>
+          {'\n\n'}
+          Je voudrais des informations sur les traitement médicamenteux :{' '}
+          <Text
+            style={styles.link}
+            onPress={() => {
+              logEvents.logInfoClick('reseau_pic');
+              Linking.openURL(
+                'http://www.reseau-pic.info/?dest=fiches/nom.php',
+              );
+            }}>
+            reseau-pic.com{' '}
+          </Text>
+          {'\n\n'}
+          Je suis étudiant et j'aimerais consulter gratuitement un psychologue :{' '}
+          <Text
+            style={styles.link}
+            onPress={() => {
+              logEvents.logInfoClick('reseau_pic');
+              Linking.openURL('https://santepsy.etudiant.gouv.fr/');
+            }}>
+            santepsy.etudiant.gouv.fr{' '}
+          </Text>
+        </Text>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  containerInfos: {
+    backgroundColor: 'rgba(38,56,124, 0.03)',
+    borderRadius: 10,
+    borderWidth: 0,
+    paddingTop: 25,
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingBottom: 30,
+    display: 'flex',
+    flexDirection: 'row',
+    marginVertical: 40,
+  },
+  lessButton: {
+    fontWeight: '700',
+    textDecorationLine: 'underline',
+    color: colors.BLUE,
+    paddingTop: 10,
+  },
+  moreButton: {
+    fontWeight: '700',
+    textDecorationLine: 'underline',
+    color: colors.BLUE,
+    paddingTop: 10,
+  },
   safe: {
     flex: 1,
   },
