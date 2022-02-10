0f4bcde8eb4ffe4332a1c714de417e48e92ffb87
tangimds <tangimds@gmail.com>
2022-02-10 16:21:57 +0100

    chore: eslint

--- src/components/TextTag.js
+++ src/components/TextTag.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import {StyleSheet, TouchableOpacity, View} from 'react-native';
-import {colors} from '../utils/colors';
-import Text from './MyText';
-import Icon from './Icon';
+import React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { colors } from "../utils/colors";
+import Text from "./MyText";
+import Icon from "./Icon";
 
 export default ({
   textColor = colors.DARK_BLUE,
@@ -14,44 +14,37 @@ export default ({
   textStyle,
   value,
   selected,
-  color = '#1FC6D5',
+  color = "#1FC6D5",
   enableClosed = false,
   enableAdd = false,
 }) => {
   let backgroundColor = `${color}66`;
   if (selected) backgroundColor = colors.DARK_BLUE;
-  if (disabled) backgroundColor = 'lightgrey';
+  if (disabled) backgroundColor = "lightgrey";
 
   let borderColor = color;
   if (selected) borderColor = colors.DARK_BLUE;
-  if (disabled) borderColor = 'grey';
+  if (disabled) borderColor = "grey";
 
   let myTextColor = textColor;
-  if (selected) myTextColor = 'white';
-  if (disabled) myTextColor = 'grey';
+  if (selected) myTextColor = "white";
+  if (disabled) myTextColor = "grey";
   return (
     <View style={styles.container}>
       <TouchableOpacity
-        style={[{...styles.button, backgroundColor, borderColor}, buttonStyle]}
+        style={[{ ...styles.button, backgroundColor, borderColor }, buttonStyle]}
         onPress={() => onPress(value)}
-        disabled={disabled}>
-        <Text style={[{...styles.text, color: myTextColor}, textStyle]}>
-          {value}
-        </Text>
+        disabled={disabled}
+      >
+        <Text style={[{ ...styles.text, color: myTextColor }, textStyle]}>{value}</Text>
       </TouchableOpacity>
       {enableClosed && (
-        <TouchableOpacity
-          style={styles.close}
-          onPress={() => onClose(value)}
-          disabled={disabled}>
+        <TouchableOpacity style={styles.close} onPress={() => onClose(value)} disabled={disabled}>
           <Icon icon="CrossSvg" width={8} height={8} color={colors.BLUE} />
         </TouchableOpacity>
       )}
       {enableAdd && (
-        <TouchableOpacity
-          style={styles.add}
-          onPress={() => onAdd(value)}
-          disabled={disabled}>
+        <TouchableOpacity style={styles.add} onPress={() => onAdd(value)} disabled={disabled}>
           <Icon icon="CrossSvg" width={8} height={8} color={colors.BLUE} />
         </TouchableOpacity>
       )}
@@ -60,55 +53,55 @@ export default ({
 };
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    alignSelf: 'flex-end',
+    display: "flex",
+    alignSelf: "flex-end",
     marginBottom: 10,
     marginRight: 20,
   },
   close: {
-    position: 'absolute',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: "absolute",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     top: -5,
     right: -8,
-    backgroundColor: '#F4FCFD',
+    backgroundColor: "#F4FCFD",
     borderRadius: 16,
-    borderColor: '#D4F0F2',
+    borderColor: "#D4F0F2",
     borderWidth: 1,
     zIndex: 2,
     width: 18,
     height: 18,
   },
   add: {
-    position: 'absolute',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: "absolute",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     top: -5,
     right: -8,
-    backgroundColor: '#F4FCFD',
+    backgroundColor: "#F4FCFD",
     borderRadius: 16,
-    borderColor: '#D4F0F2',
+    borderColor: "#D4F0F2",
     borderWidth: 1,
     zIndex: 2,
     width: 18,
     height: 18,
-    transform: [{rotate: '45deg'}],
+    transform: [{ rotate: "45deg" }],
   },
   button: {
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
     minHeight: 38,
     borderRadius: 10,
     paddingHorizontal: 15,
     paddingVertical: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     borderWidth: 1,
   },
   text: {
-    fontWeight: 'normal',
+    fontWeight: "normal",
     fontSize: 15,
   },
-  closeText: {fontSize: 12},
+  closeText: { fontSize: 12 },
 });
