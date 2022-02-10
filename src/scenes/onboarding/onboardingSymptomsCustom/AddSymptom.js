dc63244f70914cab04680ff667fd9fadb1e3642f
tangimds <tangimds@gmail.com>
2022-02-10 16:21:13 +0100

    chore:eslint

--- src/scenes/onboarding/onboardingSymptomsCustom/AddSymptom.js
+++ src/scenes/onboarding/onboardingSymptomsCustom/AddSymptom.js
@@ -1,19 +1,12 @@
-import React, {useState} from 'react';
-import {
-  StyleSheet,
-  View,
-  TouchableOpacity,
-  TextInput,
-  Keyboard,
-  Platform,
-} from 'react-native';
-import {colors} from '../../../utils/colors';
-import CircledIcon from '../../../components/CircledIcon';
-import Button from '../../../components/RoundButtonIcon';
+import React, { useState } from "react";
+import { StyleSheet, View, TouchableOpacity, TextInput, Keyboard, Platform } from "react-native";
+import { colors } from "../../../utils/colors";
+import CircledIcon from "../../../components/CircledIcon";
+import Button from "../../../components/RoundButtonIcon";
 
 const AddSymptom = ({
   onChange = console.log,
-  placeholder = 'Ajouter...',
+  placeholder = "Ajouter...",
   styleContainer,
   onChangeText = console.log,
 }) => {
@@ -24,9 +17,9 @@ const AddSymptom = ({
       <View style={styles.inputContainer}>
         <TextInput
           multiline={true}
-          numberOfLines={Platform.OS === 'ios' ? null : 3}
-          minHeight={Platform.OS === 'ios' ? 30 * 3 : null}
-          textAlignVertical={'top'}
+          numberOfLines={Platform.OS === "ios" ? null : 3}
+          minHeight={Platform.OS === "ios" ? 30 * 3 : null}
+          textAlignVertical={"top"}
           onChangeText={(e) => {
             setValue(e);
             onChangeText(e);
@@ -46,8 +39,8 @@ const AddSymptom = ({
             onPress={() => {
               Keyboard.dismiss();
               onChange(value);
-              setValue('');
-              onChangeText('');
+              setValue("");
+              onChangeText("");
             }}
           />
         </View>
@@ -58,42 +51,42 @@ const AddSymptom = ({
 
 const styles = StyleSheet.create({
   buttonsContainer: {
-    width: '100%',
-    display: 'flex',
-    position: 'absolute',
+    width: "100%",
+    display: "flex",
+    position: "absolute",
     bottom: -25,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   iconContainer: {
-    display: 'flex',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
     marginRight: 8,
-    transform: [{rotate: '180deg'}],
+    transform: [{ rotate: "180deg" }],
   },
   inputContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    height: '100%',
+    display: "flex",
+    flexDirection: "row",
+    height: "100%",
     flex: 1,
     borderWidth: 0.5,
-    borderColor: '#D4F0F2',
-    backgroundColor: '#F4FCFD',
+    borderColor: "#D4F0F2",
+    backgroundColor: "#F4FCFD",
     borderRadius: 8,
     color: colors.DARK_BLUE,
   },
   container: {
     marginVertical: 15,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   text: {
     fontSize: 16,
     paddingVertical: 12,
     paddingHorizontal: 10,
-    textAlign: 'left',
+    textAlign: "left",
     color: colors.DARK_BLUE,
     flex: 1,
   },
