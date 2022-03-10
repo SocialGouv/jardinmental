4dd49c51c6d957eea904501a38db762c60f260bd
tangimds <tangimds@gmail.com>
2022-03-10 14:25:12 +0100

    chore: eslint

--- src/scenes/status/utils/index.js
+++ src/scenes/status/utils/index.js
@@ -1,5 +1,5 @@
-import {beforeToday} from '../../../utils/date/helpers';
-import {compareAsc, parseISO} from 'date-fns';
+import { beforeToday } from "../../../utils/date/helpers";
+import { compareAsc, parseISO } from "date-fns";
 
 export const canEdit = (d) => {
   const limitDate = beforeToday(7);
