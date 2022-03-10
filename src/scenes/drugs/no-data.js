a8bb14b30327f48e5dac30c7638f2b237ce16e7e
tangimds <tangimds@gmail.com>
2022-03-10 14:17:20 +0100

    chore:eslint

--- src/scenes/drugs/no-data.js
+++ src/scenes/drugs/no-data.js
@@ -1,14 +1,14 @@
-import React, {useContext} from 'react';
-import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import React, { useContext } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 
-import {DiaryDataContext} from '../../context/diaryData';
-import Text from '../../components/MyText';
-import {colors} from '../../utils/colors';
-import Button from '../../components/Button';
-import localStorage from '../../utils/localStorage';
-import {alertNoDataYesterday} from '../survey/survey-data';
+import { DiaryDataContext } from "../../context/diaryData";
+import Text from "../../components/MyText";
+import { colors } from "../../utils/colors";
+import Button from "../../components/Button";
+import localStorage from "../../utils/localStorage";
+import { alertNoDataYesterday } from "../survey/survey-data";
 
-export default ({navigation, route}) => {
+export default ({ navigation, route }) => {
   const [diaryData] = useContext(DiaryDataContext);
 
   const handleNoTreatment = async () => {
@@ -18,25 +18,25 @@ export default ({navigation, route}) => {
       diaryData,
       navigation,
     });
-    navigation.navigate('tabs');
+    navigation.navigate("tabs");
   };
   const handleDrugInformation = async () => {
-    navigation.navigate('onboarding-drugs-information', {onboarding: true});
+    navigation.navigate("onboarding-drugs-information", { onboarding: true });
   };
 
   return (
     <View>
       <Text style={styles.subtitle}>
-        Je suis chaque jour mes <Text style={styles.lightblue}>prises</Text> de{' '}
-        <Text style={styles.lightblue}>traitement</Text>, cela me permet de{' '}
-        <Text style={styles.lightblue}>comprendre</Text> comment il influe mon{' '}
+        Je suis chaque jour mes <Text style={styles.lightblue}>prises</Text> de{" "}
+        <Text style={styles.lightblue}>traitement</Text>, cela me permet de{" "}
+        <Text style={styles.lightblue}>comprendre</Text> comment il influe mon{" "}
         <Text style={styles.lightblue}>état</Text>
       </Text>
       <Button
-        onPress={() => navigation.navigate('drugs-list')}
+        onPress={() => navigation.navigate("drugs-list")}
         title="Ajouter un traitement"
         buttonStyle={styles.button}
-        textStyle={{fontSize: 14, fontWeight: 'bold'}}
+        textStyle={{ fontSize: 14, fontWeight: "bold" }}
       />
       <TouchableOpacity onPress={handleDrugInformation}>
         <Text style={styles.link}>Informations sur les traitements</Text>
@@ -46,34 +46,34 @@ export default ({navigation, route}) => {
         onPress={handleNoTreatment}
         title="Je ne prends pas de traitement"
         buttonStyle={styles.darkButton}
-        textStyle={{fontSize: 14, fontWeight: 'bold'}}
+        textStyle={{ fontSize: 14, fontWeight: "bold" }}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {marginTop: 40, height: 60, alignSelf: 'center'},
+  button: { marginTop: 40, height: 60, alignSelf: "center" },
   darkButton: {
     marginTop: 40,
     backgroundColor: colors.DARK_BLUE,
     height: 60,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
   subtitle: {
     color: colors.DARK_BLUE,
     fontSize: 18,
     marginBottom: 10,
-    fontWeight: '300',
-    textAlign: 'center',
+    fontWeight: "300",
+    textAlign: "center",
   },
   link: {
-    color: '#181818',
-    textDecorationLine: 'underline',
+    color: "#181818",
+    textDecorationLine: "underline",
     fontSize: 14,
     marginBottom: 10,
-    fontWeight: '300',
-    textAlign: 'center',
+    fontWeight: "300",
+    textAlign: "center",
   },
   lightblue: {
     color: colors.LIGHT_BLUE,
--- src/scenes/drugs/not-found.js
+++ src/scenes/drugs/not-found.js
@@ -1,29 +1,27 @@
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, View, TextInput, TouchableOpacity} from 'react-native';
-import Text from '../../components/MyText';
-import {colors} from '../../utils/colors';
-import Button from '../../components/Button';
-import localStorage from '../../utils/localStorage';
-import Matomo from '../../services/matomo';
-import logEvents from '../../services/logEvents';
-import {sendTipimail} from '../../services/sendTipimail';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, TextInput, TouchableOpacity } from "react-native";
+import Text from "../../components/MyText";
+import { colors } from "../../utils/colors";
+import Button from "../../components/Button";
+import localStorage from "../../utils/localStorage";
+import Matomo from "../../services/matomo";
+import logEvents from "../../services/logEvents";
+import { sendTipimail } from "../../services/sendTipimail";
 
-export default ({navigation, searchedValue}) => {
+export default ({ navigation, searchedValue }) => {
   const [drugNotFound, setDrugNotFound] = useState();
   const [npsSent, setNpsSent] = useState(false);
 
   useEffect(() => {
-    setDrugNotFound(
-      `Je ne trouve pas mon traitement, Je souhaiterais ajouter "${searchedValue}"`,
-    );
+    setDrugNotFound(`Je ne trouve pas mon traitement, Je souhaiterais ajouter "${searchedValue}"`);
   }, [searchedValue]);
 
   const handleNoTreatment = async () => {
     await localStorage.setMedicalTreatment([]);
-    navigation.navigate('tabs');
+    navigation.navigate("tabs");
   };
 
-  const formatText = ({drugNotFound, userId}) => {
+  const formatText = ({ drugNotFound, userId }) => {
     let text = `User: ${userId}\n`;
     text += `Indiquez nous, si vous le souhaitez, le traitement que vous souhaiteriez suivre: ${drugNotFound}\n`;
     return text;
@@ -38,13 +36,13 @@ export default ({navigation, searchedValue}) => {
     await sendTipimail(
       {
         from: {
-          address: 'contact@monsuivipsy.fr',
-          personalName: 'MonSuiviPsy - Application',
+          address: "contact@monsuivipsy.fr",
+          personalName: "MonSuiviPsy - Application",
         },
-        subject: 'MonSuiviPsy - NPS',
-        text: formatText({drugNotFound, userId}),
+        subject: "MonSuiviPsy - NPS",
+        text: formatText({ drugNotFound, userId }),
       },
-      __DEV__ ? 'tangimds@gmail.com' : 'monsuivipsy@fabrique.social.gouv.fr',
+      __DEV__ ? "tangimds@gmail.com" : "monsuivipsy@fabrique.social.gouv.fr"
     );
     setNpsSent(true);
   };
@@ -55,14 +53,11 @@ export default ({navigation, searchedValue}) => {
         {`Aucun résultat ne correspond à cette recherche : "${searchedValue}"`}
       </Text>
       {npsSent ? (
-        <Text style={styles.topSubTitle}>
-          Merci, nous avons bien pris en compte votre retour !
-        </Text>
+        <Text style={styles.topSubTitle}>Merci, nous avons bien pris en compte votre retour !</Text>
       ) : (
         <>
           <Text style={styles.topSubTitle}>
-            Indiquez nous, si vous le souhaitez, le traitement que vous
-            souhaiteriez suivre.
+            Indiquez nous, si vous le souhaitez, le traitement que vous souhaiteriez suivre.
           </Text>
           <TextInput
             style={styles.feedback}
@@ -88,8 +83,8 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     marginVertical: 20,
-    alignItems: 'flex-start',
-    alignSelf: 'center',
+    alignItems: "flex-start",
+    alignSelf: "center",
     flexGrow: 0,
     marginBottom: 150,
     backgroundColor: colors.BLUE,
@@ -98,83 +93,83 @@ const styles = StyleSheet.create({
     borderRadius: 50,
   },
   buttonText: {
-    color: '#dbdbe9',
+    color: "#dbdbe9",
   },
 
   topTitle: {
-    width: '95%',
+    width: "95%",
     flexShrink: 0,
     marginTop: 10,
     color: colors.BLUE,
   },
   topSubTitle: {
-    width: '95%',
+    width: "95%",
     flexShrink: 0,
     marginTop: 35,
-    color: '#191919',
+    color: "#191919",
   },
   feedback: {
-    width: '100%',
+    width: "100%",
     height: 100,
     borderRadius: 7,
     borderWidth: 1,
-    borderColor: '#dbdbe9',
-    backgroundColor: '#f3f3f6',
+    borderColor: "#dbdbe9",
+    backgroundColor: "#f3f3f6",
     padding: 15,
     marginTop: 15,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
   },
 
   notFound: {
-    color: '#444',
-    fontStyle: 'italic',
-    fontWeight: '600',
+    color: "#444",
+    fontStyle: "italic",
+    fontWeight: "600",
     padding: 30,
   },
 
   separator: {
     borderTopWidth: 1,
-    borderColor: '#EDEDED',
+    borderColor: "#EDEDED",
     marginVertical: 30,
-    alignItems: 'center',
+    alignItems: "center",
   },
   separatorText: {
     top: -10,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingHorizontal: 20,
-    color: '#8F8F8F',
-    fontStyle: 'italic',
-    fontWeight: '600',
+    color: "#8F8F8F",
+    fontStyle: "italic",
+    fontWeight: "600",
   },
   card: {
-    display: 'flex',
-    flexDirection: 'row',
-    backgroundColor: '#F4FCFD',
-    borderColor: '#d4f0f2',
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "#F4FCFD",
+    borderColor: "#d4f0f2",
     borderWidth: 1,
     borderRadius: 10,
     padding: 10,
   },
-  cardContent: {flex: 1},
-  button: {width: '90%'},
+  cardContent: { flex: 1 },
+  button: { width: "90%" },
   cardTitle: {
     fontSize: 15,
     color: colors.DARK_BLUE,
-    fontWeight: '500',
+    fontWeight: "500",
     marginBottom: 10,
   },
   cardSubTitle: {
     fontSize: 14,
     color: colors.DARK_BLUE,
-    fontWeight: '300',
+    fontWeight: "300",
     marginBottom: 10,
   },
   buttonWrapper: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
     paddingVertical: 10,
   },
 });
