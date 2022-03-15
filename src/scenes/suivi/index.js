a2af41bd611ef2d562b4cc1c08f78957640d593b
tangimds <tangimds@gmail.com>
2022-03-15 18:08:28 +0100

    fix(camembert): smiley moyen + pourcentage

--- src/scenes/suivi/index.js
+++ src/scenes/suivi/index.js
@@ -14,8 +14,7 @@ import Courbes from "../calendar/calendar";
 import logEvents from "../../services/logEvents";
 
 const screenHeight = Dimensions.get("window").height;
-// const CHART_TYPES = ["Frises", "Diagrammes", "Courbes", "Évènements"];
-const CHART_TYPES = ["Frises", "Courbes", "Évènements"];
+const CHART_TYPES = ["Frises", "Statistiques", "Courbes", "Évènements"];
 
 const nextChartType = (chartType) => {
   const i = CHART_TYPES.indexOf(chartType);
@@ -44,7 +43,7 @@ const Suivi = ({ navigation }) => {
 
   const renderChart = (chart) => {
     switch (chart) {
-      case "Diagrammes":
+      case "Statistiques":
         return <ChartPie fromDate={fromDate} toDate={toDate} />;
       case "Courbes":
         return <Courbes navigation={navigation} />;
