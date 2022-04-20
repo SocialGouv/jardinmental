c6bd8f03d223ff5a9811ca77faa5a41771a9d43f
tangimds <tangimds@gmail.com>
2022-04-20 11:28:40 +0200

    chore: eslint

--- src/scenes/status/no-data-status-item.js
+++ src/scenes/status/no-data-status-item.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import Text from '../../components/MyText';
-import {colors} from '../../utils/colors';
-import {canEdit} from './utils/index.js';
-import ArrowRightSvg from '../../../assets/svg/arrow-right.js';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Text from "../../components/MyText";
+import { colors } from "../../utils/colors";
+import { canEdit } from "./utils/index.js";
+import ArrowRightSvg from "../../../assets/svg/arrow-right.js";
 
-const NoDataDiaryItem = ({date}) => {
+const NoDataDiaryItem = ({ date }) => {
   return (
     <View style={styles.textContainer}>
       <Text style={styles.noDataTitle}>
         {canEdit(date)
-          ? 'Renseigner mon état pour ce jour-là'
-          : 'Je ne peux plus saisir mon questionnaire pour ce jour'}
+          ? "Renseigner mon état pour ce jour-là"
+          : "Je ne peux plus saisir mon questionnaire pour ce jour"}
       </Text>
       {canEdit(date) && <ArrowRightSvg color="#C7CED5" />}
     </View>
@@ -20,22 +20,22 @@ const NoDataDiaryItem = ({date}) => {
 
 const styles = StyleSheet.create({
   noDataContainer: {
-    backgroundColor: 'rgba(38, 56, 124, 0.03)',
+    backgroundColor: "rgba(38, 56, 124, 0.03)",
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: 'rgba(38, 56, 124, 0.08)',
-    display: 'flex',
+    borderColor: "rgba(38, 56, 124, 0.08)",
+    display: "flex",
     marginBottom: 20,
   },
   noDataTitle: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: colors.BLUE,
   },
   textContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     paddingHorizontal: 15,
   },
 });
