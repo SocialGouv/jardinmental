3d057fc55c50ae4c752f396836c5866ae5e9f238
tangimds <tangimds@gmail.com>
2022-05-20 15:28:38 +0200

    chore: eslint

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
-import React, {useState} from 'react';
-import {StyleSheet, View, Dimensions} from 'react-native';
-import {colors} from '../utils/colors';
-import Icon from './Icon';
-import Text from './MyText';
-import Settings from '../scenes/settings/settings-modal';
-import Drawer from './drawer';
-import {useRoute, useFocusEffect} from '@react-navigation/native';
-import {needUpdate} from '../services/versionChecker';
-import {getBadgeNotesVersion} from '../scenes/news';
-import localStorage from '../utils/localStorage';
+import React, { useState } from "react";
+import { StyleSheet, View, Dimensions } from "react-native";
+import { colors } from "../utils/colors";
+import Icon from "./Icon";
+import Text from "./MyText";
+import Settings from "../scenes/settings/settings-modal";
+import Drawer from "./drawer";
+import { useRoute, useFocusEffect } from "@react-navigation/native";
+import { needUpdate } from "../services/versionChecker";
+import { getBadgeNotesVersion } from "../scenes/news";
+import localStorage from "../utils/localStorage";
 
-const Header = ({title, navigation}) => {
+const Header = ({ title, navigation }) => {
   const [settingsVisible, setSettingsVisible] = useState(false);
   const [drawerVisible, setDrawerVisible] = useState();
   const [badge, setBadge] = useState(false);
@@ -21,22 +21,18 @@ const Header = ({title, navigation}) => {
     const news = await getBadgeNotesVersion();
     const supported = await localStorage.getSupported();
     const badgeProNPS = await localStorage.getVisitProNPS();
-    setBadge(update || news || (supported === 'PRO' && !badgeProNPS));
+    setBadge(update || news || (supported === "PRO" && !badgeProNPS));
   };
 
   useFocusEffect(
     React.useCallback(() => {
       updateBadge();
-    }, []),
+    }, [])
   );
 
   return (
     <View style={styles.container}>
-      <Settings
-        visible={settingsVisible}
-        navigation={navigation}
-        onClick={() => setSettingsVisible(false)}
-      />
+      <Settings visible={settingsVisible} navigation={navigation} onClick={() => setSettingsVisible(false)} />
       <Drawer
         visible={drawerVisible}
         navigation={navigation}
@@ -51,7 +47,7 @@ const Header = ({title, navigation}) => {
         width={20}
         height={20}
         onPress={() => setDrawerVisible(true)}
-        styleContainer={{marginRight: 20}}
+        styleContainer={{ marginRight: 20 }}
       />
       <Text style={styles.title}>{title}</Text>
       <Icon
@@ -71,18 +67,18 @@ const styles = StyleSheet.create({
     // borderColor: 'blue',
     // borderWidth: 1,
     flex: 0,
-    backgroundColor: 'transparent',
-    marginRight: 'auto',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
+    backgroundColor: "transparent",
+    marginRight: "auto",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
     paddingBottom: 5,
   },
   title: {
-    fontSize: Dimensions.get('window').width > 380 ? 19 : 16,
+    fontSize: Dimensions.get("window").width > 380 ? 19 : 16,
     color: colors.BLUE,
-    marginRight: 'auto',
-    fontWeight: 'bold',
+    marginRight: "auto",
+    fontWeight: "bold",
     flex: 1,
   },
 });
