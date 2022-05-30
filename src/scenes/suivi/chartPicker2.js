b0a0eb37c1d9567cd5a463cb20f1e3f8c2371be9
tangimds <tangimds@gmail.com>
2022-05-30 10:27:29 +0200

    up

--- src/scenes/suivi/chartPicker2.js
+++ src/scenes/suivi/chartPicker2.js
@@ -1,41 +1,74 @@
 import React from "react";
-import { View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import { View, StyleSheet, TouchableOpacity, ScrollView, FlatList } from "react-native";
 import { colors } from "../../utils/colors";
 import Text from "../../components/MyText";
 import logEvents from "../../services/logEvents";
 
-const CHART_TYPES = ["Frises", "Statistiques", "Courbes", "Déclencheurs"];
+const CHART_TYPES = [
+  {
+    key: "Frises",
+    label: "Frises",
+  },
+  {
+    key: "Statistiques",
+    label: "Statistiques",
+  },
+  {
+    key: "Courbes",
+    label: "Courbes",
+  },
+  {
+    key: "Déclencheurs",
+    label: "Déclencheurs",
+  },
+];
 
 const TabPicker = ({ onChange, ongletActif = "Frises" }) => {
+  const listRef = React.useRef();
+
   const handlePress = (tab) => {
     // logEvents.logStatusSubPage(tab);
     onChange(tab);
   };
 
   return (
-    <ScrollView
+    <FlatList
+      ref={listRef}
+      data={CHART_TYPES}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          key={item.key}
+          onPress={() => {
+            listRef.current?.scrollToItem({
+              item,
+              animated: true,
+              viewPosition: 0.5,
+            });
+            handlePress(item.label);
+          }}
+          style={[tabStyles.tabButtonContainer]}
+        >
+          <View
+            style={[
+              tabStyles.tabButtonContainer,
+              ongletActif === item.label ? tabStyles.tabActif : tabStyles.tabInactif,
+            ]}
+          >
+            <Text
+              style={[tabStyles.button, ongletActif === item.label ? tabStyles.actif : tabStyles.inactif]}
+            >
+              {item.label}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      )}
       horizontal
       style={tabStyles.scrollView}
       contentContainerStyle={tabStyles.scrollContainer}
       fadingEdgeLength={10}
       showsHorizontalScrollIndicator={false}
       snapToAlignment={"end"}
-    >
-      {CHART_TYPES.map((tab) => (
-        <TouchableOpacity key={tab} onPress={() => handlePress(tab)} style={[tabStyles.tabButtonContainer]}>
-          <View
-            style={[
-              tabStyles.tabButtonContainer,
-              ongletActif === tab ? tabStyles.tabActif : tabStyles.tabInactif,
-            ]}
-          >
-            <Text style={[tabStyles.button, ongletActif === tab ? tabStyles.actif : tabStyles.inactif]}>
-              {tab}
-            </Text>
-          </View>
-        </TouchableOpacity>
-      ))}
-    </ScrollView>
+    />
   );
 };
 
