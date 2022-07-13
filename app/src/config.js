5a74d6def58ff8395699df3673f3d6965db53714
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-07-13 11:51:29 +0200

    feat: env indicator to app

--- app/src/config.js
+++ app/src/config.js
@@ -3,9 +3,10 @@ import { version, buildNumber } from "../package.json";
 
 const SCHEME = envConfig.SCHEME;
 const HOST = envConfig.HOST;
+const APP_ENV = envConfig.APP_ENV;
 const VERSION = version;
 const BUILD_NUMBER = buildNumber;
 const TIPIMAIL_API_KEY = envConfig.TIPIMAIL_API_KEY;
 const TIPIMAIL_API_USER = envConfig.TIPIMAIL_API_USER;
 
-export { SCHEME, HOST, VERSION, BUILD_NUMBER, TIPIMAIL_API_KEY, TIPIMAIL_API_USER };
+export { SCHEME, HOST, APP_ENV, VERSION, BUILD_NUMBER, TIPIMAIL_API_KEY, TIPIMAIL_API_USER };
