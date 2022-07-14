dab3e4857a9cea4c9ce95b5dfd754c627157a8db
tangimds <tangimds@gmail.com>
2022-07-14 15:20:48 +0200

    fix: enable prod test inapp

--- api/src/controllers/event.js
+++ api/src/controllers/event.js
@@ -34,29 +34,28 @@ router.post(
     // if (body.event?.category === "IN_APP_CLICK" && body.event?.action === "COMMENT_CLICK") {
     //   return res.status(200).send({ ok: true });
     // }
-    //   if (body.event?.category === "APP" && body.event?.action === "APP_OPEN") {
-    //     return res.status(200).send({
-    //       ok: true,
-    //       sendInApp: [
-    //         "Bienvenue dans les analyses !",
-    //         "Vous pourrez voir ici les mesures de vos saisies quotidiennes.",
-    //         // [
-    //         //   {
-    //         //     text: "Pourquoi ?",
-    //         //     navigate: ["HEALTH"],
-    //         //   },
-    //         //   {
-    //         //     text: "Comment ?",
-    //         //     navigate: ["CONSO_FOLLOW_UP"],
-    //         //     style: "destructive",
-    //         //     event: { category: "IN_APP_CLICK", action: "COMMENT_CLICK" },
-    //         //   },
-    //         // ],
-    //         // { cancelable: true },
-    //       ],
-    //     });
-    //   }
-    //   return res.status(200).send({ ok: true });
+      if (body.event?.category === "OPEN_TAB" && body.event?.action === "CALENDAR_OPEN") {
+        return res.status(200).send({
+          ok: true,
+          sendInApp: [
+            "Bienvenue dans les analyses !",
+            "Vous pourrez voir ici les mesures de vos saisies quotidiennes.",
+            // [
+            //   {
+            //     text: "Pourquoi ?",
+            //     navigate: ["HEALTH"],
+            //   },
+            //   {
+            //     text: "Comment ?",
+            //     navigate: ["CONSO_FOLLOW_UP"],
+            //     style: "destructive",
+            //     event: { category: "IN_APP_CLICK", action: "COMMENT_CLICK" },
+            //   },
+            // ],
+            // { cancelable: true },
+          ],
+        });
+      }
     return res.status(200).send({ ok: true });
   })
 );
