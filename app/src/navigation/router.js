11745792eb6ae48bc06a36e2a0bec77864608ae6
theolemague <60031565+theolemague@users.noreply.github.com>
2022-07-28 17:43:15 +0200

    fix: écrans explications analyses (#181)
    * ajout écrant explication onboarding
    
    * Update router.js
    
    * png to svg
    
    Co-authored-by: Tangi Mendès <tangimds@gmail.com>

--- app/src/navigation/router.js
+++ app/src/navigation/router.js
@@ -17,6 +17,7 @@ import OnboardingSymptomsCustom from "../scenes/onboarding/onboardingSymptomsCus
 import OnboardingDrugs from "../scenes/onboarding/onboardingDrugs";
 import OnboardingDrugsInformation from "../scenes/onboarding/onboardingDrugs/drugs-information";
 import OnboardingDrugsList from "../scenes/onboarding/onboardingDrugs/list";
+import OnboardingExplanation from "../scenes/onboarding/onboardingExplanation";
 import CGU from "../scenes/legal/cgu-screen";
 import Privacy from "../scenes/legal/privacy-screen";
 import LegalMentions from "../scenes/legal/legal-mentions-screen";
@@ -72,52 +73,53 @@ class Router extends React.Component {
   render() {
     return (
       <>
-      <NavigationContainer ref={(r) => (this.navigationRef = r)} onStateChange={this.onStateChange}>
-        <Stack.Navigator initialRouteName="tabs" headerMode="none">
-          <Stack.Screen name="day-survey" component={DaySurveyScreen} />
-          <Stack.Screen name="select-day" component={SelectDayScreen} />
-          <Stack.Screen name="tabs" component={Tabs} />
-          {/* <Stack.Screen
+        <NavigationContainer ref={(r) => (this.navigationRef = r)} onStateChange={this.onStateChange}>
+          <Stack.Navigator initialRouteName="onboarding-stats" headerMode="none">
+            <Stack.Screen name="day-survey" component={DaySurveyScreen} />
+            <Stack.Screen name="select-day" component={SelectDayScreen} />
+            <Stack.Screen name="tabs" component={Tabs} />
+            {/* <Stack.Screen
             name="question"
             options={{animationEnabled: Platform.OS === 'ios'}}>
             {({navigation, route}) => (
               <SurveyScreen navigation={navigation} route={route} />
             )}
           </Stack.Screen> */}
-          <Stack.Screen name="symptoms">
-            {(props) => <OnboardingSymptomsCustom settings={true} {...props} />}
-          </Stack.Screen>
-          <Stack.Screen name="reminder" component={Reminder} />
-          <Stack.Screen name="export" component={Export} />
-          <Stack.Screen name="chart-day" component={DailyChart} />
-          <Stack.Screen name="notes" options={{ animationEnabled: Platform.OS === "ios" }}>
-            {({ navigation, route }) => <Notes navigation={navigation} route={route} />}
-          </Stack.Screen>
-          <Stack.Screen name="onboarding" component={Onboarding} />
-          <Stack.Screen name="onboarding-symptoms" component={OnboardingSymptoms} />
-          <Stack.Screen name="onboarding-symptoms-custom" component={OnboardingSymptomsCustom} />
-          <Stack.Screen name="onboarding-drugs" component={OnboardingDrugs} />
-          <Stack.Screen name="onboarding-drugs-information" component={OnboardingDrugsInformation} />
-          <Stack.Screen name="onboarding-drugs-list" component={OnboardingDrugsList} />
-          <Stack.Screen name="supported" component={Supported} />
-          <Stack.Screen name="cgu" component={CGU} />
-          <Stack.Screen name="privacy" component={Privacy} />
-          <Stack.Screen name="legal-mentions" component={LegalMentions} />
-          <Stack.Screen name="drugs" component={Drugs} />
-          <Stack.Screen name="drugs-list" component={DrugsList} />
-          <Stack.Screen name="too-late" component={TooLate} />
-          <Stack.Screen name="news" component={News} />
-          <Stack.Screen name="infos" component={Infos} />
-          <Stack.Screen name="contact" component={Contact} />
-          <Stack.Screen name="privacy-light" component={PrivacyLight} />
-          <Stack.Screen name="contribute-pro" component={ContributePro} />
-          <Stack.Screen name="activate-beck" component={ActivateBeck} />
-          <Stack.Screen name="view-beck" component={ViewBeck} />
-          <Stack.Screen name="beck" component={Beck} />
-          {/* <Stack.Screen name="contribute" component={Contribute} /> */}
-        </Stack.Navigator>
-      </NavigationContainer>
-      <EnvironmentIndicator />
+            <Stack.Screen name="symptoms">
+              {(props) => <OnboardingSymptomsCustom settings={true} {...props} />}
+            </Stack.Screen>
+            <Stack.Screen name="reminder" component={Reminder} />
+            <Stack.Screen name="export" component={Export} />
+            <Stack.Screen name="chart-day" component={DailyChart} />
+            <Stack.Screen name="notes" options={{ animationEnabled: Platform.OS === "ios" }}>
+              {({ navigation, route }) => <Notes navigation={navigation} route={route} />}
+            </Stack.Screen>
+            <Stack.Screen name="onboarding" component={Onboarding} />
+            <Stack.Screen name="onboarding-symptoms" component={OnboardingSymptoms} />
+            <Stack.Screen name="onboarding-symptoms-custom" component={OnboardingSymptomsCustom} />
+            <Stack.Screen name="onboarding-drugs" component={OnboardingDrugs} />
+            <Stack.Screen name="onboarding-drugs-information" component={OnboardingDrugsInformation} />
+            <Stack.Screen name="onboarding-drugs-list" component={OnboardingDrugsList} />
+            <Stack.Screen name="onboarding-stats" component={OnboardingExplanation} />
+            <Stack.Screen name="supported" component={Supported} />
+            <Stack.Screen name="cgu" component={CGU} />
+            <Stack.Screen name="privacy" component={Privacy} />
+            <Stack.Screen name="legal-mentions" component={LegalMentions} />
+            <Stack.Screen name="drugs" component={Drugs} />
+            <Stack.Screen name="drugs-list" component={DrugsList} />
+            <Stack.Screen name="too-late" component={TooLate} />
+            <Stack.Screen name="news" component={News} />
+            <Stack.Screen name="infos" component={Infos} />
+            <Stack.Screen name="contact" component={Contact} />
+            <Stack.Screen name="privacy-light" component={PrivacyLight} />
+            <Stack.Screen name="contribute-pro" component={ContributePro} />
+            <Stack.Screen name="activate-beck" component={ActivateBeck} />
+            <Stack.Screen name="view-beck" component={ViewBeck} />
+            <Stack.Screen name="beck" component={Beck} />
+            {/* <Stack.Screen name="contribute" component={Contribute} /> */}
+          </Stack.Navigator>
+        </NavigationContainer>
+        <EnvironmentIndicator />
       </>
     );
   }
---
+++ app/src/scenes/onboarding/onboardingExplanation.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect, useRef } from "react";
+import { StyleSheet, SafeAreaView, View, ScrollView, Image, Dimensions } from "react-native";
+
+import Text from "../../components/MyText";
+import { colors } from "../../utils/colors";
+import logEvents from "../../services/logEvents";
+import Button from "../../components/Button";
+import BackButton from "../../components/BackButton";
+
+import PhoneViewTriggers from "../../../assets/svg/PhoneViewTriggers";
+import PhoneViewCurves from "../../../assets/svg/PhoneViewCurves";
+import PhoneViewStats from "../../../assets/svg/PhoneViewStats";
+import PhoneViewFriezes from "../../../assets/svg/PhoneViewFriezes";
+
+const OnboardingExplanation = ({ navigation }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = async () => {
+    currentIndex < 3 ? setCurrentIndex(currentIndex + 1) : setCurrentIndex(3)
+    logEvents.logOnboardingExplainNext(currentIndex);
+  };
+  const handleStart = async () => {
+    logEvents.logOnboardingExplainStart();
+    navigation.navigate("tabs");
+  };
+
+  return (
+    <SafeAreaView style={styles.safe}>
+      <BackButton onPress={navigation.goBack} />
+      <Text style={styles.title}>Apprendre à mieux se connaître au fil du temps</Text>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        {currentIndex === 0 && <ScreenStats />}
+        {currentIndex === 1 && <ScreenFriezes />}
+        {currentIndex === 2 && <ScreenCurves />}
+        {currentIndex === 3 && <ScreenTriggers />}
+      </ScrollView>
+      <View style={styles.CTAButtonContainer}>
+        <View style={styles.buttonWrapper}>
+          {
+            currentIndex === 3 ? <Button title="Je démarre" onPress={handleStart} />
+              : <Button title="Suivant" onPress={handleNext} />
+          }
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const ScreenStats = () => (
+  <View style={styles.container}>
+    <Text style={styles.presentationText}>
+      Les <Text style={styles.bold}>statistiques</Text> vous donneront un bilan de chaque indicateur sur la période de votre choix, ainsi que votre observance de vos prises de traitement
+    </Text>
+    <PhoneViewStats style={styles.image} />
+  </View>
+);
+
+const ScreenFriezes = () => (
+  <View style={styles.container}>
+    <Text style={styles.presentationText}>
+      Les <Text style={styles.bold}>frises</Text> vous permettront de suivre l’évolution de vos indicateurs, de comprendre les corrélations entre eux, ainsi que les effets de votre traitement
+    </Text>
+    <PhoneViewFriezes style={styles.image} />
+  </View>
+);
+const ScreenCurves = () => (
+  <View style={styles.container}>
+    <Text style={styles.presentationText}>
+      Les <Text style={styles.bold}>courbes</Text> vous permettront de suivre votre évolution et de retrouver rapidement le détail de chaque journée en cliquant sur le point correspondant.
+    </Text>
+    <PhoneViewCurves style={styles.image} />
+  </View>
+);
+const ScreenTriggers = () => (
+  <View style={styles.container}>
+    <Text style={styles.presentationText}>
+      Les <Text style={styles.bold}>déclencheurs</Text> vous aideront à comprendre ce qui influe sur votre état de santé mentale, en retrouvant vos notes personnelles liées à l’indicateur et son intensité de votre choix.
+    </Text>
+    <PhoneViewTriggers style={styles.image} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  CTAButtonContainer: {},
+  scrollContainer: { flex: 1 },
+  safe: {
+    flex: 1,
+    backgroundColor: "white",
+    display: "flex",
+  },
+  imageContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+  image: {
+    marginTop: 20,
+    height: Dimensions.get("window").height * 0.6, // Not working
+    resizeMode: "contain",
+  },
+  title: {
+    color: colors.BLUE,
+    fontSize: 22,
+    padding: 20,
+    fontWeight: "700",
+    textAlign: "center",
+  },
+  container: {
+    backgroundColor: "white",
+    padding: 20,
+    display: "flex",
+    alignItems: "center"
+  },
+  buttonWrapper: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: 15,
+  },
+  bold: {
+    fontWeight: "bold",
+    color: colors.BLUE,
+  },
+  presentationText: {
+    fontSize: 20,
+    color: colors.BLUE,
+  }
+});
+
+export default OnboardingExplanation;
