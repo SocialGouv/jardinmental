83e584cadfae37ead449360fcda8f3c6843bfdb0
tangimds <tangimds@gmail.com>
2022-09-01 12:32:05 +0200

    screens onboardings

--- app/assets/svg/Support.js
+++ app/assets/svg/Support.js
@@ -5,6 +5,7 @@ const SvgComponent = (props) => (
   <Svg
     width={83}
     height={83}
+    viewBox="0 0 83 83"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
--- app/src/scenes/onboarding/screens.js
+++ app/src/scenes/onboarding/screens.js
@@ -5,7 +5,6 @@ import CheckBox from "@react-native-community/checkbox";
 import WelcomeIcon from "../../../assets/svg/WelcomeIcon";
 import Support from "../../../assets/svg/Support";
 import IllustrationOnboarding from "../../../assets/svg/IllustrationOnboarding";
-import Screen3Image from "../../../assets/svg/Chat";
 
 import { StyleSheet, View, Dimensions } from "react-native";
 export const buttonHeight = 43;
@@ -19,32 +18,36 @@ const size = screenHeight * (Dimensions.get("window").height > 700 ? 0.15 : 0.1)
 export const Screen0 = () => (
   <View style={styles.container}>
     <View style={styles.imageContainer}>
-      <WelcomeIcon height={size} width={size} style={styles.image} />
+      <WelcomeIcon height="60%" width="60%" style={styles.image} />
     </View>
-    <Text style={styles.presentationText}>
-      <Text style={styles.h1}>Bienvenue !</Text>
-      {"\n\n"}
-      Tenez <Text style={styles.bold}>chaque jour</Text> un journal de suivi des{" "}
-      <Text style={styles.bold}>indicateurs</Text> de votre état de{" "}
-      <Text style={styles.bold}>santé mentale</Text>
-    </Text>
+    <View style={styles.containerBottom}>
+      <Text style={styles.presentationText}>
+        <Text style={styles.h1}>Bienvenue !</Text>
+        {"\n\n"}
+        Tenez <Text style={styles.bold}>chaque jour</Text> un journal de suivi des{" "}
+        <Text style={styles.bold}>indicateurs</Text> de votre état de{" "}
+        <Text style={styles.bold}>santé mentale</Text>
+      </Text>
+    </View>
   </View>
 );
 export const Screen1 = () => (
   <View style={styles.container}>
     <View style={styles.imageContainer}>
-      <IllustrationOnboarding height={size} width={size} style={[styles.image, styles.tiltUp]} />
+      <IllustrationOnboarding height="60%" width="60%" style={[styles.image]} />
     </View>
-    <Text style={styles.presentationText}>
-      <Text style={styles.h1}>Apprenez à mieux vous connaître</Text>
-      {"\n\n"}
-      En montrant vos <Text style={styles.bold}>analyses</Text> à votre{" "}
-      <Text style={styles.bold}>professionnel</Text> de santé qui pourra vous{" "}
-      <Text style={styles.bold}>accompagner</Text>
-    </Text>
+    <View style={styles.containerBottom}>
+      <Text style={styles.presentationText}>
+        <Text style={styles.h1}>Apprenez à mieux vous connaître</Text>
+        {"\n\n"}
+        En montrant vos <Text style={styles.bold}>analyses</Text> à votre{" "}
+        <Text style={styles.bold}>professionnel</Text> de santé qui pourra vous{" "}
+        <Text style={styles.bold}>accompagner</Text>
+      </Text>
+    </View>
   </View>
 );
-export const Screen2 = ({ isCguChecked, setIsCguChecked }) => {
+export const Screen2 = ({ navigation, isCguChecked, setIsCguChecked }) => {
   const onCguClick = () => navigation.navigate("cgu");
   const onLegalMentionsClick = () => navigation.navigate("legal-mentions");
   const onPrivacyClick = () => navigation.navigate("privacy");
@@ -52,62 +55,60 @@ export const Screen2 = ({ isCguChecked, setIsCguChecked }) => {
   return (
     <>
       <View style={styles.container}>
-        <Support height={size} width={size} style={[styles.image, styles.tiltUp]} />
-        <Text style={styles.presentationText}>
-          <Text style={styles.h1}>En toute confiance</Text>
-          {"\n\n"}
-          C’est <Text style={styles.bold}>gratuit, anonyme</Text> et sans{" "}
-          <Text style={styles.bold}>aucune récupération</Text> de vos saisies personnelles
-        </Text>
-        <View style={styles.cgu}>
-          <CheckBox
-            animationDuration={0.2}
-            tintColor="#1FC6D5"
-            tintColors={{ true: "#1FC6D5", false: "grey" }}
-            boxType="square"
-            style={styles.checkbox}
-            value={isCguChecked}
-            onValueChange={(newValue) => setIsCguChecked(newValue)}
-          />
-          <Text style={styles.textCgu}>
-            En cochant cette case, vous acceptez nos{" "}
-            <Text onPress={onCguClick} style={styles.underlined}>
-              Conditions Générales d’Utilisation
-            </Text>
-            , notre{" "}
-            <Text onPress={onPrivacyClick} style={styles.underlined}>
-              Politique de Confidentialité
-            </Text>{" "}
-            et nos{" "}
-            <Text onPress={onLegalMentionsClick} style={styles.underlined}>
-              Mentions Légales
-            </Text>
+        <View style={styles.imageContainer}>
+          <Support height="60%" width="60%" style={[styles.image]} />
+        </View>
+        <View style={styles.containerBottom}>
+          <Text style={styles.presentationText}>
+            <Text style={styles.h1}>En toute confiance</Text>
+            {"\n\n"}
+            C’est <Text style={styles.bold}>gratuit, anonyme</Text> et sans{" "}
+            <Text style={styles.bold}>aucune récupération</Text> de vos saisies personnelles
           </Text>
+          <View style={styles.cgu}>
+            <CheckBox
+              animationDuration={0.2}
+              tintColor="#1FC6D5"
+              tintColors={{ true: "#1FC6D5", false: "grey" }}
+              boxType="square"
+              style={styles.checkbox}
+              value={isCguChecked}
+              onValueChange={(newValue) => setIsCguChecked(newValue)}
+            />
+            <Text style={styles.textCgu}>
+              En cochant cette case, vous acceptez nos{" "}
+              <Text onPress={onCguClick} style={styles.underlined}>
+                Conditions Générales d’Utilisation
+              </Text>
+              , notre{" "}
+              <Text onPress={onPrivacyClick} style={styles.underlined}>
+                Politique de Confidentialité
+              </Text>{" "}
+              et nos{" "}
+              <Text onPress={onLegalMentionsClick} style={styles.underlined}>
+                Mentions Légales
+              </Text>
+            </Text>
+          </View>
         </View>
       </View>
     </>
   );
 };
-export const Screen3 = () => (
-  <View style={styles.container}>
-    <Screen3Image style={styles.image} />
-    <Text style={styles.presentationText}>
-      <Text style={styles.bold}>Arriver préparé</Text>
-      {"\n\n"}Le jour de ma consultation, j'ai une <Text style={styles.emphasis}>vision complète</Text> de ce
-      que j'ai vécu et de ce qui m'a interrogé pour en <Text style={styles.emphasis}>parler</Text> avec mon
-      thérapeute.
-    </Text>
-  </View>
-);
 
 const styles = StyleSheet.create({
+  containerBottom: {
+    flex: 1,
+    backgroundColor: "white",
+    marginBottom: Dimensions.get("window").height > 700 ? 20 : 60,
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+  },
   container: {
     flex: 1,
     backgroundColor: "white",
-    padding: 20,
-    marginBottom: Dimensions.get("window").height > 700 ? 20 : 60,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "stretch",
   },
   emphasis: {
     color: "#1FC6D5",
@@ -127,25 +128,26 @@ const styles = StyleSheet.create({
   },
   presentationText: {
     textAlign: "center",
-    marginTop: 20,
+    padding: 20,
     fontSize: Dimensions.get("window").height > 700 ? 20 : 17,
     color: "#0A215C",
+    display: "flex",
   },
   imageContainer: {
+    flex: 1,
     display: "flex",
     flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
   image: {
     color: "#C3C7D5",
     marginVertical: 0,
   },
-  tiltUp: {
-    marginTop: -0.1 * size,
-  },
   cgu: {
-    paddingTop: 20,
     display: "flex",
     flexDirection: "row",
+    padding: 10,
   },
   textCgu: {
     flex: 1,
