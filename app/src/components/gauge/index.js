834eac8a5021e481beebc73c5991f8776d23ba15
vi-dot <Vi-dot@users.noreply.github.com>
2022-12-16 19:11:59 +0100

    feat: gauge svg (#230)

--- app/src/components/gauge/index.js
+++ app/src/components/gauge/index.js
@@ -6,6 +6,7 @@ import { Slider } from "@miblanchard/react-native-slider";
 
 import { StyleSheet, View, Platform } from "react-native";
 import { screenWidth } from "../../scenes/onboarding/screens";
+import { GaugeChart } from "./GaugeChart";
 const HEIGHT_RATIO_GAUGE = 48 / 256;
 
 const styles = StyleSheet.create({
@@ -14,84 +15,6 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
   },
 });
-
-const Mask = ({ width, value, reverse }) => {
-  const [key, setKey] = useState(0);
-
-  // This is a hack to make the gauge work on Android
-  useEffect(() => {
-    if (Platform.OS === "android") {
-      setTimeout(() => setKey((prev) => prev + 1), 0);
-    }
-  }, []);
-
-  const numberOfBars = 20;
-  const widthBar = (width / (numberOfBars - 1)) * 0.75;
-  const marginRightBar = (width - numberOfBars * widthBar) / (numberOfBars - 1);
-  const arrayBarsIndex = [...Array(numberOfBars).keys()];
-  const widthGreyMask = width - width * value;
-
-  const colors = reverse
-    ? ["#5DEE5A", "#F2F478", "#F2F478", "#F16B6B"]
-    : ["#F16B6B", "#F2F478", "#F2F478", "#5DEE5A"];
-
-  return (
-    <MaskedView
-      key={key}
-      androidRenderingMode="software"
-      style={{
-        width: "100%",
-        height: width * HEIGHT_RATIO_GAUGE,
-        flex: 1,
-        flexDirection: "row",
-      }}
-      maskElement={
-        <View
-          style={{
-            // Transparent background because mask is based off alpha channel.
-            backgroundColor: "transparent",
-            flex: 1,
-            flexDirection: "row",
-            alignItems: "flex-end",
-          }}
-        >
-          {arrayBarsIndex.map((n) => (
-            <View
-              key={n}
-              style={{
-                borderRadius: 3,
-                width: widthBar,
-                height:
-                  width * HEIGHT_RATIO_GAUGE * 0.2 +
-                  width * HEIGHT_RATIO_GAUGE * ((n * (80 / (numberOfBars - 1))) / 100), // height * 0.2 + height * [0 to 0.8]
-                backgroundColor: "#000", // backgroundColor needed to make the mask work
-                marginRight: n === arrayBarsIndex.length - 1 ? 0 : marginRightBar, // no margin right on the last one
-              }}
-            />
-          ))}
-        </View>
-      }
-    >
-      <LinearGradient
-        colors={colors}
-        angle={90}
-        useAngle={true}
-        locations={[0, 0.479167, 0.526042, 1]}
-        style={{ flex: 1 }}
-      />
-      <View
-        style={{
-          width: widthGreyMask,
-          height: "100%",
-          zIndex: 100,
-          position: "absolute",
-          right: 0,
-          backgroundColor: "#D9DBE0",
-        }}
-      />
-    </MaskedView>
-  );
-};
 
 const Gauge = ({ hideSlider = false, defaultValue = 0, onChange, reverse }) => {
   const [value, setValue] = useState(defaultValue);
@@ -114,7 +37,7 @@ const Gauge = ({ hideSlider = false, defaultValue = 0, onChange, reverse }) => {
       }}
     >
       <View style={styles.gaugeContainer}>
-        <Mask width={width} value={value} reverse={reverse} />
+        <GaugeChart value={value} reverse={reverse} />
       </View>
       {/* <Text>{defaultValue}</Text> */}
       {hideSlider ? null : (
