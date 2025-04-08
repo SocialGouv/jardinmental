85c85e5a0c3da3c072382879d5340e4fc51ef60e
Tangi Mendès <tangimds@gmail.com>
2025-04-08 09:21:19 +0200

    chore: add assets

--- app/assets/svg/SurveyMenu.js
+++ app/assets/svg/SurveyMenu.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Svg, {Path} from 'react-native-svg';
 
 function SvgComponent(props) {
-  console.log(props);
   return (
     <Svg width={20} height={27} viewBox="0 0 20 27" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <Path
