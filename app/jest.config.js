a98272cdb58056e5085e15a82d233916c414aa85
Lucas Charrier <lucharrier@gmail.com>
2025-06-04 10:39:06 +0200

    ci: add test to ci

---
+++ app/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/android/',
+    '<rootDir>/ios/',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-.*|@react-navigation|expo.*|@expo.*|react-native-uuid|@react-native-async-storage|react-native-localize|date-fns)/)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testEnvironment: 'node',
+  moduleNameMapping: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      'identity-obj-proxy',
+  },
+};
