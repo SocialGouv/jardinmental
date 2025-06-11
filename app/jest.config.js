29918350643a0c1648013ad4b7808971a1ee4fbd
Charrier <lucharrier@gmail.com>
2025-06-11 09:06:09 +0200

    chore: remove comments

--- app/jest.config.js
+++ app/jest.config.js
@@ -17,7 +17,6 @@ module.exports = {
     '^.+\\.svg$': '<rootDir>/jest-svg-transformer.js'
   },
   moduleNameMapper: {
-    // SVG supprimé de cette ligne car géré par le transformateur
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'identity-obj-proxy',
   },
