7ae28e026de185e713b04f272200cbb92ecdd9d0
Lucas Charrier <lucharrier@gmail.com>
2025-07-17 16:42:22 +0200

    feat: add gradiant

--- app/src/components/onboarding/NavigationButtons.tsx
+++ app/src/components/onboarding/NavigationButtons.tsx
@@ -4,6 +4,7 @@ import { TW_COLORS } from '@/utils/constants';
 import JMButton from '../JMButton';
 import ChevronIcon from '@assets/svg/icon/chevron';
 import CircleQuestionMark from '@assets/svg/icon/CircleQuestionMark'
+import LinearGradient from 'react-native-linear-gradient';
 
 // Navigation Props
 interface NavigationButtonsProps {
@@ -38,7 +39,9 @@ export const NavigationButtons: React.FC<NavigationButtonsProps> = ({
   headerContent
 }) => {
   return (
-    <View
+    <LinearGradient
+      colors={['rgba(255,255,255,0)', 'rgba(255,255,255,1)']}
+      locations={[0, 0.3]} // transition très rapide
       style={absolute ? {
         position: 'absolute',
         bottom: 0,
@@ -76,28 +79,8 @@ export const NavigationButtons: React.FC<NavigationButtonsProps> = ({
           loading={loading}
           disabled={nextDisabled}
         />
-        {/* <TouchableOpacity 
-        onPress={onNext}
-        disabled={nextDisabled || loading}
-        className="px-6 py-3 rounded-lg items-center justify-center w-full"
-        style={{ 
-          backgroundColor: nextDisabled ? TW_COLORS.GRAY_LIGHT : TW_COLORS.PRIMARY,
-          opacity: nextDisabled ? 0.6 : 1
-        }}
-      >
-        {loading ? (
-          <ActivityIndicator color={TW_COLORS.WHITE} size="small" />
-        ) : (
-          <Text 
-            className="text-base font-semibold"
-            style={{ color: TW_COLORS.WHITE }}
-          >
-            {nextText}
-          </Text>
-        )}
-      </TouchableOpacity> */}
       </View>
-    </View>
+    </LinearGradient>
   );
 };
 
