3ee01240340ec1687e7b28f013596719ea995742
Lucas Charrier <lucharrier@gmail.com>
2025-08-11 10:43:31 +0200

    chore: add svg assets

---
+++ app/assets/svg/icon/CircleCheckMark.tsx
@@ -0,0 +1,18 @@
+import Svg, { ClipPath, Defs, G, Path, Rect } from "react-native-svg";
+
+export default function CircleCheckMark() {
+    return <Svg width="16" height="17" viewBox="0 0 16 17" fill="none">
+        <G clip-path="url(#clip0_17585_11417)">
+            <Path d="M4.99967 8.49967L6.99967 10.4997L10.9997 6.49967M14.6663 8.49967C14.6663 12.1816 11.6816 15.1663 7.99967 15.1663C4.31778 15.1663 1.33301 12.1816 1.33301 8.49967C1.33301 4.81778 4.31778 1.83301 7.99967 1.83301C11.6816 1.83301 14.6663 4.81778 14.6663 8.49967Z"
+            stroke="#12747D"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"/>
+        </G>
+        <Defs>
+            <ClipPath id="clip0_17585_11417">
+                <Rect width="16" height="16" fill="white" transform="translate(0 0.5)"/>
+            </ClipPath>
+        </Defs>
+    </Svg>
+}
---
+++ app/assets/svg/icon/PuzzlePiece.tsx
@@ -0,0 +1,23 @@
+
+import Svg, { Defs, G, Path, Rect, ClipPath } from "react-native-svg";
+
+export default function PuzzlePiece({
+    color
+}: {
+    color: string
+}) {
+    return <Svg width="20" height="20" viewBox="0 0 20 20" fill="none">
+        <G clip-path="url(#clip0_17592_674)">
+            <Path d="M9.99984 1.6665L12.9998 4.6665C14.9998 -0.583496 20.5832 4.99984 15.3332 6.99984L18.3332 9.99984L15.3332 12.9998C13.3332 7.74984 7.74984 13.3332 12.9998 15.3332L9.99984 18.3332L6.99984 15.3332C4.99984 20.5832 -0.583496 14.9998 4.6665 12.9998L1.6665 9.99984L4.6665 6.99984C6.6665 12.2498 12.2498 6.6665 6.99984 4.6665L9.99984 1.6665Z"
+            stroke={color || "#617778"}
+            strokeWidth="1.66667"
+            strokeLinecap="round"
+            strokeLinejoin="round"/>
+        </G>
+        <Defs>
+            <ClipPath id="clip0_17592_674">
+                <Rect width="20" height="20" fill="white"/>
+            </ClipPath>
+        </Defs>
+        </Svg>
+}
