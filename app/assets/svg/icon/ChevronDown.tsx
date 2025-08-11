abb595efe6a3828a206a303622c15ef5185e0741
Charrier <lucharrier@gmail.com>
2025-08-11 10:44:12 +0200

    chore: add svg assets (#329)

---
+++ app/assets/svg/icon/ChevronDown.tsx
@@ -0,0 +1,13 @@
+import Svg, { Path } from "react-native-svg";
+
+export default function ChevronDown({
+    color
+}) {
+    return <Svg width="21" height="20" viewBox="0 0 21 20" fill="none">
+        <Path d="M5.5 7.5L10.5 12.5L15.5 7.5"
+        stroke={color || "#158993"}
+        strokeWidth="1.66667"
+        strokeLinecap="round"
+        strokeLinejoin="round"/>
+    </Svg>
+}
