796511e85d84f5fcf0c8ed1f78b49ca6052cea46
Charrier <lucharrier@gmail.com>
2025-08-27 11:58:55 +0200

    Update app/src/scenes/onboarding-v2/checkIn/mood/OnboardingCheckInHowDoYouFeelDetailsScreen.tsx
    Co-authored-by: Revu <dnum.fabrique@sg.social.gouv.fr>

--- app/src/scenes/onboarding-v2/checkIn/mood/OnboardingCheckInHowDoYouFeelDetailsScreen.tsx
+++ app/src/scenes/onboarding-v2/checkIn/mood/OnboardingCheckInHowDoYouFeelDetailsScreen.tsx
@@ -139,7 +139,7 @@ export const OnboardingCheckInLastMoods: React.FC<Props> = ({ navigation, route 
     try {
       const date = formatDay(beforeToday(0));
       const prev = diaryData[date] || {};
-      const key = INDICATEURS_HUMEUR.uuid;
+      const key = getIndicatorKey(INDICATEURS_HUMEUR);
       const updatedAnswers = {
         ...prev,
         [key]: {
