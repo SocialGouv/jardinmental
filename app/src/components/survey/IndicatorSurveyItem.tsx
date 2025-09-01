b16a2db27726f8d7be325b6840e909028939988f
Charrier <lucharrier@gmail.com>
2025-09-01 10:10:12 +0200

    Update app/src/components/survey/IndicatorSurveyItem.tsx
    Co-authored-by: Revu <dnum.fabrique@sg.social.gouv.fr>

--- app/src/components/survey/IndicatorSurveyItem.tsx
+++ app/src/components/survey/IndicatorSurveyItem.tsx
@@ -53,7 +53,15 @@ export const IndicatorSurveyItem = ({
       });
       setAddedIndicators(indicators);
       // in this case indicators maximum one element, that is the one to display
+  const addIndicatorForCategory = async (category: NEW_INDICATORS_CATEGORIES, indicators: PredefineIndicatorV2SchemaType[]) => {
+    for (const indicatorItem of indicators) {
+      await localStorage.replaceOrAddIndicateur({
+        ...generateIndicatorFromPredefinedIndicator(indicatorItem),
+        baseIndicatorUuid: indicatorItem.uuid,
+        uuid: indicator.uuid, // we keep the same uuid for tracking purpose in the stats
+      });
     }
+    setAddedIndicators(indicators);
     if (indicators.length > 0) {
       setDisplayedName(indicators[0].name);
     } else {
