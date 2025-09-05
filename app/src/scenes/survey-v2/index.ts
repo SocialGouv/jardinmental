a9d9eb30875388e5f59ad79ed588edbd8ee1a8ab
Raphaël Huchet <raph@selego.co>
2025-09-05 14:30:55 +0200

    fix: remove unused files

--- app/src/scenes/survey-v2/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as SurveyNavigator } from "./SurveyNavigator";
-export { IndicatorScreen } from "./IndicatorScreen";
-export { GoalsScreen } from "./GoalsScreen";
-export { ContextScreen } from "./ContextScreen";
-export { ToxicScreen } from "./ToxicScreen";
-export { SurveyProgressBar } from "../../components/survey/SurveyProgressBar";
-export { useSurveyScreens } from "./hooks/useSurveyScreens";
