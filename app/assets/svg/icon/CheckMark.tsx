c6ca9ccc1083720d357b5f0dce21ea5fb2553ad8
Raphaël Huchet <raphaelht@gmail.com>
2025-09-08 10:10:45 +0200

    fix: delete unused svg (#438)

--- app/assets/svg/icon/CheckMark.tsx
+++
@@ -1,9 +0,0 @@
-import Svg, { Path } from "react-native-svg";
-
-export default function CheckIcon() {
-  return (
-    <Svg width="20" height="20" viewBox="0 0 20 20" fill="none">
-      <Path d="M16.6663 5L7.49967 14.1667L3.33301 10" stroke="#21896B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    </Svg>
-  );
-}
