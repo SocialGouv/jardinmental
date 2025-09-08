f03578b0eacf1bd083718b1d14e26f38aa8da025
Lucas Charrier <lucharrier@gmail.com>
2025-09-08 14:18:32 +0200

    chore: allow scrolling in view in landspace mode

--- app/src/components/DrugsBottomSheet.tsx
+++ app/src/components/DrugsBottomSheet.tsx
@@ -1,6 +1,6 @@
 import { TW_COLORS } from "@/utils/constants";
 import Health from "@assets/svg/icon/Health";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, useWindowDimensions, Dimensions } from "react-native";
 import JMButton from "./JMButton";
 import ArrowIcon from "@assets/svg/icon/Arrow";
 import { mergeClassNames } from "@/utils/className";
@@ -9,18 +9,25 @@ import CircleQuestionMark from "@assets/svg/icon/CircleQuestionMark";
 import { useEffect, useState } from "react";
 import Drugs from "@/scenes/drugs/drugs-list";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import HelpText from "./HelpText";
 import HelpView from "./HelpView";
 import { useBottomSheet } from "@/context/BottomSheetContext";
 import localStorage from "@/utils/localStorage";
 import { HELP_POSOLOGY } from "@/scenes/onboarding-v2/data/helperData";
 import { Drug } from "@/entities/Drug";
 
+const screenHeight = Dimensions.get("window").height;
+const height90vh = screenHeight * 0.9;
+
 export const DrugsBottomSheet = ({ onClose }: { onClose: (treatment?: Drug[]) => void }) => {
   const [index, setIndex] = useState<number>(0);
-  const { showBottomSheet } = useBottomSheet();
+  const { showBottomSheet, closeBottomSheet } = useBottomSheet();
   const [treatment, setTreatment] = useState<any[] | undefined>();
-  const { closeBottomSheet } = useBottomSheet();
+
+  const { width, height } = useWindowDimensions();
+  const isLandscape = width > height;
+
+  // wrapper: View en portrait, ScrollView en paysage
+  const ContentWrapper = isLandscape ? ScrollView : View;
 
   const handleNoTreatment = async () => {
     await localStorage.setMedicalTreatment([]);
@@ -46,15 +53,19 @@ export const DrugsBottomSheet = ({ onClose }: { onClose: (treatment?: Drug[]) =>
   return (
     <View className="flex-1 bg-white px-4">
       {index === 0 && (
-        <View>
-          <View>
-            <View className={`flex-row items-center p-4 px-0 pb-6`}>
-              <View className="rounded-full border-[1.5px] border-cnam-primary-800 bg-white w-10 h-10 items-center justify-center">
-                <Health />
-              </View>
+        <ContentWrapper
+          contentContainerStyle={isLandscape ? { paddingBottom: 100 } : null}
+          showsVerticalScrollIndicator={false}
+          style={isLandscape ? { paddingVertical: 20, height: height90vh } : null}
+        >
+          <View className="flex-row items-center p-4 px-0 pb-6">
+            <View className="rounded-full border-[1.5px] border-cnam-primary-800 bg-white w-10 h-10 items-center justify-center">
+              <Health />
             </View>
           </View>
+
           <Text className={mergeClassNames(typography.textLgBold, "text-xl mb-4 text-cnam-primary-950")}>Prenez-vous un traitement ?</Text>
+
           <TouchableOpacity
             onPress={() => {
               showBottomSheet(
@@ -66,10 +77,12 @@ export const DrugsBottomSheet = ({ onClose }: { onClose: (treatment?: Drug[]) =>
             <CircleQuestionMark color={TW_COLORS.GRAY_400} />
             <Text className={mergeClassNames(typography.textSmSemibold, "ml-2 text-gray-600")}>Information sur les traitements</Text>
           </TouchableOpacity>
+
           <Text className={mergeClassNames(typography.textMdMedium, "text-cnam-primary-800")}>
             La prise d’un traitement peut avoir un effet sur votre quotidien. Suivre la prise de traitement permet de mieux comprendre son effet sur
             votre état de santé mentale
           </Text>
+
           <JMButton
             onPress={() => setIndex(1)}
             title="Oui, j’ai un traitement"
@@ -79,8 +92,9 @@ export const DrugsBottomSheet = ({ onClose }: { onClose: (treatment?: Drug[]) =>
           />
           <JMButton onPress={handleNoTreatment} title="Non, je n'ai pas de traitement" className="mt-4" />
           <JMButton onPress={handleLater} variant="text" title="Je le ferai plus tard" className="mt-4" />
-        </View>
+        </ContentWrapper>
       )}
+
       {index === 1 && <Drugs navigation={{ goBack: () => {} }} route={undefined} onClose={onClose} />}
     </View>
   );
