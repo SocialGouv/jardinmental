1d238606475906d3cba51ff0db201d7484aff206
Lucas Charrier <lucharrier@gmail.com>
2025-09-11 14:51:30 +0200

    chore: add new faq screens

---
+++ app/src/scenes/faq/FaqData.tsx
@@ -0,0 +1,173 @@
+import Pencil from "@assets/svg/Pencil";
+
+export const FAQ_DATA = {
+  indicateurs: {
+    title: "Définir mes indicateurs",
+    subtitle: `Qu'est-ce qu'un indicateur ?`,
+    description: `Un indicateur est un repère sur l’état de votre santé mentale. Cela peut être une émotion, un symptôme ou un comportement.
+    \nSuivre leur évolution vous permettra de mieux comprendre ce que vous traversez et d’identifier ce qui influence vos hauts et vos bas.`,
+    exemple: `_**_Par exemple_** : si vous dormez mal, vous pourriez suivre la « qualité de votre sommeil » et le « nombre de réveils nocturnes »._`,
+    accordion: [
+      {
+        title: "Comment les choisir ?",
+        description: `Concentrez-vous sur les indicateurs qui ont eu un impact sur votre bien-être **ces dernières semaines**.
+        \nNous vous conseillons de ne pas choisir plus de 9 indicateurs. Vous serez plus régulier dans votre suivi et cela vous sera plus utile pour comprendre votre fonctionnement !`,
+      },
+      {
+        title: `Comment les paramétrer ?`,
+        description: `Vous pouvez accéder aux paramétres de vos indicateurs soit lorsque vous remplissez votre questionnaire quotidien, soit depuis les *Paramètres généraux* (⚙️ en haut à droite) en cliquant sur « *Personnaliser mes indicateurs* » : 
+- Pour **modifier vos indicateurs existants** : cliquez sur « *Modifier mon questionnaire* ». Vous pourrez alors changer l’ordre d’apparition de vos indicateurs ou en supprimer en cliquant sur la corbeille 🗑️ .
+- Pour **créer un nouvel indicateur** : cliquez sur le bouton « Ajouter un nouvel indicateur ». Vous pourrez alors définir le nom de votre indicateur et choisir son échelle d’évaluation. Vous pouvez vous inspirer des exemples proposés ou réactiver un indicateur archivé.`,
+      },
+    ],
+    next: {
+      icon: <Pencil />,
+      label: "Définir mes indicateurs",
+      path: "faq-detail",
+    },
+  },
+  objectifs: {
+    title: "Définir mes objectifs",
+    subtitle: `Qu'est-ce qu'un objectif`,
+    description: `Un objectif est une petit défi que vous souhaitez réaliser régulièrement pour améliorer ou stabiliser votre bien-être. Cela peut être une activité comme marcher 30 minutes par jour, faire un exercice de respiration ou encore une habitude de soin comme prendre son traitement.`,
+    // exemple: `_**_Par exemple_** : si vous dormez mal, vous pourriez suivre la « qualité de votre sommeil » et le « nombre de réveils nocturnes »._`,
+    accordion: [
+      {
+        title: "Comment les choisir ?",
+        description: `Commencez par 1 ou 2 objectifs seulement et fixez-vous des objectifs réalistes. C’est beaucoup plus motivant de cocher régulièrement ses réussites que de se décourager avec des objectifs inatteignables ! Vous pouvez aussi ajuster leur fréquence : un objectif peut être quotidien, mais aussi 2 fois par semaine ou seulement le week-end, selon ce qui vous convient. 
+\nLeur but est de vous aider à progresser pas à pas, pas de bousculer toutes vos habitudes d’un coup 😊`,
+      },
+      {
+        title: `Comment les paramétrer ?`,
+        description: `Vous pouvez accéder aux paramétres de vos objectifs soit lorsque vous remplissez votre questionnaire quotidien, soit depuis les *Paramètres généraux* (⚙️ en haut à droite) en cliquant sur « *Personnaliser mes objectifs* » : 
+- Pour **créer un nouvel objectif** : cliquez sur le bouton « Ajouter un objectif ». Vous pourrez alors définir le nom de votre objectif. Vous pouvez vous inspirer des exemples proposés ou réactiver un objectif archivé.
+- Pour **modifier vos objectifs existants** :
+    - Si vous souhaitez modifier la récurrence d’un objectif existant ou activer / éteindre les rappels, cliquez sur l’icône du stylo 🖊️ à côté de l’objectif en question.
+    - Si vous souhaitez supprimer un objectif existant, cliquez sur le bouton « Modifier mes objectifs » puis cliquez sur la corbeille 🗑️ à côté de l’objectif en question.`,
+      },
+    ],
+    next: {
+      icon: <Pencil />,
+      label: "Définir mes indicateurs",
+      path: "faq-detail",
+    },
+  },
+  questionnaire: {
+    title: "Faire mon suivi quotidien",
+    subtitle: `Qu'est-ce qu'un objectif`,
+    description: `Chaque jour, prenez quelques minutes pour faire le point sur votre santé mentale.`,
+    // exemple: `_**_Par exemple_** : si vous dormez mal, vous pourriez suivre la « qualité de votre sommeil » et le « nombre de réveils nocturnes »._`,
+    accordion: [
+      {
+        title: "Comment remplir mon  questionnaire ?",
+        description: `Pour cela, rendez-vous dans l’onglet « Mes Entrées » et cliquez sur « Renseigner mon état pour ce jour-là ».`,
+      },
+      {
+        title: `Que puis-je renseigner chaque jour?`,
+        description: `Vous pourrez :
+
+- évaluer vos indicateurs ;
+- cocher les objectifs atteints ;
+- ajouter une note pour préciser un événement marquant ;
+- indiquer si vous avez consommé des substances ;
+- confirmer la prise de votre traitement (si vous en avez renseigné un) ;
+- noter une prise de médicament “si besoin”.
+`,
+      },
+      {
+        title: `Jusqu’à combien de jours en arrière puis-je compléter ?`,
+        description: `Vous pouvez remplir ou compléter votre questionnaire jusqu’à **7 jours en arrière**. Passé ce délai, il ne sera plus possible de le faire car les souvenirs deviennent moins fiables et risquent de fausser vos données.
+
+👉 **La régularité est essentielle** : plus vous prenez l’habitude de remplir votre questionnaire chaque jour, plus vous aurez une vision claire de ce qui influence votre état — que ce soit positivement ou négativement.`,
+      },
+    ],
+    next: {
+      icon: <Pencil />,
+      label: "Définir mes indicateurs",
+      path: "faq-detail",
+    },
+  },
+  analyse: {
+    title: "Comprendre mes analyses",
+    // subtitle: `Qu'est-ce qu'un objectif`,
+    description: `Découvrez comment vos observations sont transformées en analyses pour mieux suivre votre état au fil du temps.`,
+    // exemple: `_**_Par exemple_** : si vous dormez mal, vous pourriez suivre la « qualité de votre sommeil » et le « nombre de réveils nocturnes »._`,
+    accordion: [
+      {
+        title: "À quoi servent mes analyses ?",
+        description: `C’est le cœur de l’application : vos analyses vous permettent d’observer vos données, de mieux comprendre ce qui influence votre état et de repérer ce qui vous aide (ou au contraire vous fragilise).`,
+      },
+      {
+        title: `Comment interpréter mes données ?`,
+        description: `Vous pourrez :
+
+- évaluer vos indicateurs ;
+- cocher les objectifs atteints ;
+- ajouter une note pour préciser un événement marquant ;
+- indiquer si vous avez consommé des substances ;
+- confirmer la prise de votre traitement (si vous en avez renseigné un) ;
+- noter une prise de médicament “si besoin”.
+`,
+      },
+      {
+        title: `Jusqu’à combien de jours en arrière puis-je compléter ?`,
+        description: `Vous pouvez remplir ou compléter votre questionnaire jusqu’à **7 jours en arrière**. Passé ce délai, il ne sera plus possible de le faire car les souvenirs deviennent moins fiables et risquent de fausser vos données.
+
+👉 **La régularité est essentielle** : plus vous prenez l’habitude de remplir votre questionnaire chaque jour, plus vous aurez une vision claire de ce qui influence votre état — que ce soit positivement ou négativement.`,
+      },
+    ],
+    next: {
+      icon: <Pencil />,
+      label: "Définir mes indicateurs",
+      path: "faq-detail",
+    },
+  },
+  traitement: {
+    title: "Comprendre mes analyses",
+    // subtitle: `Qu'est-ce qu'un objectif`,
+    description: `Jardin Mental vous permet de suivre la prise de votre traitement si votre professionnel de santé vous en a prescrit un.`,
+    // exemple: `_**_Par exemple_** : si vous dormez mal, vous pourriez suivre la « qualité de votre sommeil » et le « nombre de réveils nocturnes »._`,
+    accordion: [
+      {
+        title: "Comment ajouter mon traitement ?",
+        description: `Pour renseigner votre traitement, allez dans les *Paramètre généraux* (⚙️ en haut à droite) puis cliquez sur *« Saisir mon traitement ».* 
+
+Vous pourrez alors ajouter votre traitement en sélectionnant dans la liste le nom de votre médicament, ou en ajouter un.
+
+Nous vous rappellerons de le prendre dans votre questionnaire quotidien.`,
+      },
+      {
+        title: `Puis-je créer un traitement qui n’est pas dans la liste ?`,
+        description: `Si vous ne trouvez pas votre traitement dans la liste, vous pouvez créer une nouvelle entrée en cliquant sur le symbôle ➕. `,
+      },
+    ],
+    next: {
+      icon: <Pencil />,
+      label: "Définir mes indicateurs",
+      path: "faq-detail",
+    },
+  },
+  données: {
+    title: "Partager mes données",
+    // subtitle: `Qu'est-ce qu'un objectif`,
+    description: `Cette section répond à vos questions sur le récapitulatif de vos données et son utilisation.`,
+    // exemple: `_**_Par exemple_** : si vous dormez mal, vous pourriez suivre la « qualité de votre sommeil » et le « nombre de réveils nocturnes »._`,
+    accordion: [
+      {
+        title: "À quoi sert la génération de récapitulatif de mes données ?",
+        description: `Si vous êtes suivi.e par un.e professionnel.le de santé, vous pouvez si vous le souhaitez lui partager vos données. Ces informations lui permettront d’avoir une vue d’ensemble de votre état entre vos consultations et l’aideront à adapter son accompagnement et / ou votre traitement en conséquence.`,
+      },
+      {
+        title: `Comment générer un récapitulatif de mes données ?`,
+        description: `Pour générer un récapitulatif de vos données, rendez-vous dans les *Paramètres généraux* (⚙️ en haut à droite) et cliquez sur « *Générer un récapitulatif de mes données* » sur les **30 derniers jours**. 
+
+Vous pourrez alors télécharger le fichier au format PDF.`,
+      },
+    ],
+    next: {
+      icon: <Pencil />,
+      label: "Définir mes indicateurs",
+      path: "faq-detail",
+    },
+  },
+};
--- app/src/scenes/faq/FaqMainScreen.tsx
+++ app/src/scenes/faq/FaqMainScreen.tsx
@@ -1,14 +1,19 @@
 import { mergeClassNames } from "@/utils/className";
 import { typography } from "@/utils/typography";
-import ArrowIcon from "@assets/svg/icon/Arrow";
-import CheckMarkIcon from "@assets/svg/icon/check";
-import React, { useCallback, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { Text, View } from "react-native";
 import { AnimatedHeaderScrollScreen } from "../survey-v2/AnimatedHeaderScrollScreen";
 import { TW_COLORS } from "@/utils/constants";
-import { Card } from "@/components/Card";
+import JMButton from "@/components/JMButton";
+import NavigationListItem from "@/components/ListItem/NavigationListItem";
+import Pencil from "@assets/svg/Pencil";
+import Goal from "@assets/svg/icon/Goal";
+import CalendarIcon from "@assets/svg/icon/Calendar";
+import TrendUpIcon from "@assets/svg/icon/TrendUp";
+import HealthIcon from "@assets/svg/icon/Health";
+import ShareIcon from "@assets/svg/icon/Share";
 
-export default function CheckListScreen({ navigation, route }) {
+export default function FaqMainScreen({ navigation, route }) {
   return (
     <AnimatedHeaderScrollScreen
       title={"Comment ça marche ?"}
@@ -20,56 +25,75 @@ export default function CheckListScreen({ navigation, route }) {
       showBottomButton={false}
       navigation={navigation}
     >
-      <View className="bg-gray-50 flex-1 p-4 flex-col space-y-4">
-        <View className="flex-col space-y-2">
-          <Text className={mergeClassNames(typography.displayXsSemibold, "text-cnam-primary-950 text-left mb-8")}>
+      <View className="bg-gray-50 flex-1 p-4 flex-col space-y-12 pt-10">
+        <View className="flex-col space-y-6">
+          <Text className={mergeClassNames(typography.displayXsSemibold, "text-cnam-primary-950 text-left")}>
             Jardin mental, qu’est-ce que c’est?
           </Text>
-          <Text className={mergeClassNames(typography.textMdRegular, "text-cnam-primary-900 text-left mb-8")}>
-            La santé mentale mérite d'être préservée au quotidien, pas seulement lorsqu'elle est en crise.{"\n\n"}
-            <Text className={typography.textMdSemibold}>Un bon premier pas est d'apprendre à s'observer avec bienveillance</Text>, sans tomber dans
-            l'analyse excessive. Cela aide à reconnaître les signaux de mal-être et à identifier ce qui nous apporte du bien-être.{"\n\n"}
-            Notre application Jardin Mental offre un espace pour évaluer et suivre son état mental, même dans les moments positifs.
-          </Text>
+          <View className="flex-col space-y-4">
+            <Text className={mergeClassNames(typography.textMdRegular, "text-cnam-primary-900 text-left")}>
+              La santé mentale mérite d'être préservée au quotidien, pas seulement lorsqu'elle est en crise.{"\n\n"}
+              <Text className={typography.textMdSemibold}>Un bon premier pas est d'apprendre à s'observer avec bienveillance</Text>, sans tomber dans
+              l'analyse excessive. Cela aide à reconnaître les signaux de mal-être et à identifier ce qui nous apporte du bien-être.{"\n\n"}
+              Notre application{" "}
+              <Text className={typography.textMdSemibold}>
+                Jardin Mental offre un espace pour évaluer et suivre son état mental, même dans les moments positifs.
+              </Text>
+            </Text>
+          </View>
         </View>
-        <View className="flex-col space-y-2">
-          <Text className={mergeClassNames(typography.displayXsSemibold, "text-cnam-primary-950 text-left mb-8")}>Guide Pratique</Text>
+        <View className="flex-col space-y-6">
+          <Text className={mergeClassNames(typography.displayXsSemibold, "text-cnam-primary-950 text-left")}>Guide Pratique</Text>
           <View className="flex-col space-y-1">
             {[
               {
-                icon: "",
+                icon: <Pencil />,
                 label: "Définir mes indicateurs",
-                path: "/faq/indicateur",
+                path: "faq-detail",
               },
               {
-                icon: "",
+                icon: <Goal />,
                 label: "Définir mes objectifs",
-                path: "/faq/indicateur",
+                path: "faq-detail",
               },
               {
-                icon: "",
+                icon: <CalendarIcon />,
                 label: "Faire mon suivi quotidien",
-                path: "/faq/indicateur",
+                path: "faq-detail",
               },
               {
-                icon: "",
+                icon: <TrendUpIcon />,
                 label: "Comprendre mes analyses",
-                path: "/faq/indicateur",
+                path: "faq-detail",
               },
               {
-                icon: "",
+                icon: <HealthIcon />,
                 label: "Renseigner mon traitement",
-                path: "/faq/indicateur",
+                path: "faq-detail",
               },
               {
-                icon: "",
-                label: "Parteger mes données",
-                path: "/faq/indicateur",
+                icon: <ShareIcon />,
+                label: "Partager mes données",
+                path: "faq-detail",
               },
-            ].map((test) => null)}
+            ].map((item, index) => (
+              <NavigationListItem
+                label={item.label}
+                onPress={() => {
+                  navigation.navigate(item.path);
+                }}
+                icon={item.icon}
+              />
+            ))}
           </View>
         </View>
-        <Card title="Besoin d'assistance" text={`Si vous ne trouvez pas la réponse à votre question, contactez-nous sur `} />
+        <View className={mergeClassNames("bg-blue bg-cyan-50-lighten-90 p-4 rounded-xl space-y-6 p-6")}>
+          <Text className={mergeClassNames(typography.displayXsBold, "text-cnam-primary-900 text-left")}>Besoin d'assistance ?</Text>
+          <Text className={mergeClassNames(typography.textMdRegular, "text-cnam-primary-900 text-left")}>
+            Si vous ne trouvez pas la réponse à votre question, contactez-nous sur
+          </Text>
+          <JMButton title="Contactez l’équipe de Jardin mental" variant="outline" />
+        </View>
       </View>
     </AnimatedHeaderScrollScreen>
   );
