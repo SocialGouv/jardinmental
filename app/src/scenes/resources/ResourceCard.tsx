b8401ea8cbdbed49189e5ce38daa25673917040b
Charrier <lucharrier@gmail.com>
2025-09-22 18:37:12 +0200

    chore: add minor adjustment (#486)
    * chore: add minor adjustment
    
    * chore: more height for card for iPhone SE

--- app/src/scenes/resources/ResourceCard.tsx
+++ app/src/scenes/resources/ResourceCard.tsx
@@ -14,13 +14,19 @@ const ResourceCard: React.FC<ResourceCardProps> = ({ resource, onPress }) => {
     <TouchableOpacity
       onPress={onPress}
       activeOpacity={0.7}
-      className="text-cnam-primary-950 bg-white border p-2 border-cnam-primary-400 rounded-lg flex flex-row overflow-hidden mb-2"
+      className="text-cnam-primary-950 bg-white border p-2 border-cnam-primary-400 rounded-lg flex flex-row mb-2 h-32 items-center"
     >
-      <View className="w-20 relative">
+      <View className="w-20 h-full relative">
         <Image source={resource.image} className="absolute rounded-lg inset-0 w-full h-full" resizeMode="cover" />
       </View>
-      <View className="flex-1 p-2 ml-1">
-        <Text className="text-base font-semibold mb-2 text-cnam-primary-950">{resource.title}</Text>
+      <View className="flex-1 px-3 ml-1">
+        <Text
+          numberOfLines={4} // change to how many lines you want
+          ellipsizeMode="tail"
+          className="text-base font-semibold mb-2 text-cnam-primary-950"
+        >
+          {resource.title}
+        </Text>
         {/* 
         <View className="flex flex-row items-center mt-2">
           <View className="bg-gray-200 rounded-xl py-1 px-2 flex flex-row justify-center items-center">
