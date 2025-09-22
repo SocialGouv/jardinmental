4e76d016041ca2fdc6df84fe69da474b40e829e2
Raphaël Huchet <raphaelht@gmail.com>
2025-09-22 18:43:36 +0200

    feat: resources articles (#395)
    * feat: resources articles
    
    * fix: up
    
    * fix up
    
    * fix: up
    
    * chore: add minor adjustment (#486)
    
    * chore: add minor adjustment
    
    * chore: more height for card for iPhone SE
    
    * chore: adjustment
    
    ---------
    
    Co-authored-by: Charrier <lucharrier@gmail.com>

---
+++ app/src/scenes/resources/ResourceArticle.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
+import { Resource } from "./data/resources";
+import MarkdownStyled from "./MarkdownStyled";
+import { AnimatedHeaderScrollScreen } from "../survey-v2/AnimatedHeaderScrollScreen";
+import { TW_COLORS } from "@/utils/constants";
+
+interface ResourceArticleProps {
+  navigation: any;
+  route: {
+    params: {
+      resource: Resource;
+    };
+  };
+}
+
+const ResourceArticle: React.FC<ResourceArticleProps> = ({ navigation, route }) => {
+  const { resource } = route.params;
+
+  const handleContinueReading = async () => {
+    if (!resource.source) {
+      return;
+    }
+    if (resource.source.url) {
+      try {
+        await Linking.openURL(resource.source.url);
+      } catch (error) {
+        console.error("Failed to open URL:", error);
+      }
+    }
+  };
+
+  const handleContinueReadingMore = async () => {
+    if (!resource.moreContent) {
+      return;
+    }
+    if (resource.moreContent[0].url) {
+      await Linking.openURL(resource.moreContent[0].url);
+    }
+  };
+
+  return (
+    <AnimatedHeaderScrollScreen
+      title={resource.category}
+      headerTitle="Dossier"
+      handlePrevious={() => {
+        navigation.goBack();
+      }}
+      smallHeader={true}
+      navigation={navigation}
+      showBottomButton={false}
+      scrollViewBackground={TW_COLORS.GRAY_50}
+    >
+      <View className="flex-1">
+        <View className="px-5">
+          <View className="mb-6 mt-9">
+            <Image source={resource.image} className="w-full h-[200px] rounded-2xl mb-2" resizeMode="cover" />
+          </View>
+          <Text className="text-2xl font-bold text-cnam-primary-950 mb-5 font-source-sans leading-7 text-left">{resource.title}</Text>
+
+          <MarkdownStyled markdown={resource.content} />
+
+          {resource.source ? (
+            <View className="mt-8 pt-4">
+              <Text className="text-lg text-cnam-primary-950 font-semibold mb-4 font-source-sans">Continuer la lecture</Text>
+
+              <TouchableOpacity onPress={handleContinueReading}>
+                <View className="rounded-xl p-4 flex-row items-center border border-cnam-primary-200 bg-white">
+                  <View className="flex-1 pr-2">
+                    <Text className="text-base font-semibold text-cnam-primary-950 mb-1 font-source-sans">{resource.source.title}</Text>
+                    <Text className="text-sm text-gray-500 font-source-sans leading-tight">{resource.source.text}</Text>
+                  </View>
+                  <View className="justify-center items-center">
+                    <Text className="text-lg text-cnam-primary-950 font-bold">→</Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
+          ) : null}
+          {resource.moreContent ? (
+            <View className="mt-8 pt-4">
+              <Text className="text-lg text-cnam-primary-950 font-semibold mb-4 font-source-sans">Aller plus loin</Text>
+              <View className="flex flex-col gap-4">
+                {resource.moreContent.map((item) => (
+                  <TouchableOpacity onPress={handleContinueReadingMore} key={item.title + item.text}>
+                    <View className="rounded-xl p-4 flex-row items-center border border-cnam-primary-200 bg-white">
+                      <View className="flex-1 pr-2">
+                        <Text className="text-base font-semibold text-cnam-primary-950 mb-1 font-source-sans">{item.title}</Text>
+                        <Text className="text-sm text-gray-500 font-source-sans leading-tight">{item.text}</Text>
+                      </View>
+                      <View className="justify-center items-center">
+                        <Text className="text-lg text-cnam-primary-950 font-bold">→</Text>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                ))}
+              </View>
+            </View>
+          ) : null}
+        </View>
+      </View>
+    </AnimatedHeaderScrollScreen>
+  );
+};
+
+export default ResourceArticle;
