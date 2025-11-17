1629d1babd02ca86050bc0d6ba3236bf77f48f95
Lucas Charrier <lucharrier@gmail.com>
2025-11-17 14:35:49 +0100

    chore: add orientable chevron

--- app/assets/svg/icon/chevron.tsx
+++ app/assets/svg/icon/chevron.tsx
@@ -1,9 +1,41 @@
-import Svg, { Path } from "react-native-svg";
+import Svg, { Path, G } from "react-native-svg";
 
-export default function ChevronIcon({ color, strokeWidth }: { color?: string; strokeWidth?: number }) {
+type Direction = "left" | "right" | "up" | "down";
+
+interface ChevronIconProps {
+  color?: string;
+  strokeWidth?: number;
+  direction?: Direction;
+}
+
+export default function ChevronIcon({ color, strokeWidth, direction = "left" }: ChevronIconProps) {
+  const getRotation = () => {
+    switch (direction) {
+      case "right":
+        return "180";
+      case "up":
+        return "-90";
+      case "down":
+        return "90";
+      case "left":
+      default:
+        return "0";
+    }
+  };
+
+  const rotation = getRotation();
+
   return (
     <Svg width="8" height="12" viewBox="0 0 8 12" fill="none">
-      <Path d="M6.5 11L1.5 6L6.5 1" stroke={color || "#3D6874"} strokeWidth={strokeWidth || "1.66667"} strokeLinecap="round" strokeLinejoin="round" />
+      <G origin="4, 6" rotation={rotation}>
+        <Path
+          d="M6.5 11L1.5 6L6.5 1"
+          stroke={color || "#3D6874"}
+          strokeWidth={strokeWidth || "1.66667"}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </G>
     </Svg>
   );
 }
