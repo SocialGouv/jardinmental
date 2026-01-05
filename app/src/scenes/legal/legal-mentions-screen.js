43aca071474031c83de50c21092c9d73c6dc3faa
Charrier <lucharrier@gmail.com>
2026-01-05 14:09:51 +0100

    chore: fix legal mention and add link to accessibility page (#683)

--- app/src/scenes/legal/legal-mentions-screen.js
+++ app/src/scenes/legal/legal-mentions-screen.js
@@ -7,19 +7,16 @@ const LegalMentions = ({ navigation }) => {
   const content = (
     <View className="px-2">
       <Text className="text-blue-900 text-lg font-bold my-2.5">Éditeur de l'application</Text>
+      <Text className="text-blue-900 text-base my-2">L’application Jardin Mental est édité par la Caisse nationale d’assurance maladie :</Text>
       <Text className="text-blue-900 text-base my-2">
-        L'application Jardin Mental est éditée par la Direction Générale de la Santé (DGS) au sein de la Fabrique numérique des ministères sociaux
-        située :
-      </Text>
-      <Text className="text-blue-900 text-base my-2">
-        14 avenue Duquesne{"\n"}
-        75350 Paris SP 07{"\n"}
-        France{"\n"}
-        Téléphone : 01 40 56 60 00
+        26 – 50 Immeuble Frontalis{"\n"}
+        50 avenue du Professeur André Lemierre{"\n"}
+        75986 Paris Cedex 20{"\n"}
+        Téléphone : 01 72 60 10 00
       </Text>
 
       <Text className="text-blue-900 text-lg font-bold my-2.5">Directeur de la publication</Text>
-      <Text className="text-blue-900 text-base my-2">Monsieur Grégory EMERY, directeur général de la santé</Text>
+      <Text className="text-blue-900 text-base my-2">Monsieur Thomas Fatome, directeur général</Text>
 
       <Text className="text-blue-900 text-lg font-bold my-2.5">Hébergement de l'application</Text>
       <Text className="text-blue-900 text-base my-2">Cette application est hébergée par :</Text>
@@ -31,13 +28,14 @@ const LegalMentions = ({ navigation }) => {
       </Text>
 
       <Text className="text-blue-900 text-lg font-bold my-2.5">Accessibilité</Text>
-<TouchableOpacity 
-  onPress={() => Linking.openURL("https://jardinmental.fabrique.social.gouv.fr/accessibilite")}
-  accessibilityRole="link"
-  accessibilityLabel="Consulter la déclaration d'accessibilité : non-conforme"
-  accessibilityHint="Ouvre le lien dans le navigateur">
-  <Text className="text-blue-700 underline">Accessibilité : non-conforme</Text>
-</TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => Linking.openURL("https://jardinmental.fabrique.social.gouv.fr/accessibilite")}
+        accessibilityRole="link"
+        accessibilityLabel="Consulter la déclaration d'accessibilité : non-conforme"
+        accessibilityHint="Ouvre le lien dans le navigateur"
+      >
+        <Text className="text-blue-700 underline">Accessibilité : non-conforme</Text>
+      </TouchableOpacity>
       <Text className="text-blue-900 text-base my-2">
         La conformité aux normes d'accessibilité numérique est un objectif ultérieur mais nous tâchons de rendre cette application accessible à toutes
         et à tous.
