bc8cef1d29b303c17baedb0936d9edd7c2b8b391
Charrier <lucharrier@gmail.com>
2026-01-20 12:12:40 +0100

    Update app/assets/svg/icon/HeartFull.tsx
    Co-authored-by: Revu <dnum.fabrique@sg.social.gouv.fr>

--- app/assets/svg/icon/HeartFull.tsx
+++ app/assets/svg/icon/HeartFull.tsx
@@ -1,6 +1,6 @@
 import Svg, { Path } from "react-native-svg";
 
-export default function HeartFull({ color }: { color: string }) {
+export default function HeartFull({ color }: { color?: string }) {
   return (
     <Svg width="20" height="20" viewBox="0 0 20 20" fill="none">
       <Path
