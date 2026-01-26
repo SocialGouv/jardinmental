be86df5552541ca3bd3dbbe97445e9eaae35019b
Lucas Charrier <lucharrier@gmail.com>
2026-01-26 10:43:55 +0100

    chore: add charpicker file

--- app/src/scenes/suivi/Bilan.tsx
+++ app/src/scenes/suivi/Bilan.tsx
@@ -4,7 +4,7 @@ import { useFocusEffect } from "@react-navigation/native";
 
 import { beforeToday } from "../../utils/date/helpers";
 import Header from "../../components/Header";
-import ChartPicker from "./chartPicker2";
+import ChartPicker from "./chartPicker";
 import RangeDate from "./RangeDate";
 import ChartPie from "./chartPie";
 import Evenements from "./triggers/triggers";
